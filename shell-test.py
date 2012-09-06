4adfba6e1f6648873d2d62aba127851c0deb4538
William Henney <will@henney.org>
2012-09-05 23:02:42 -0500

    Tests of proplyd vs isotropic wind
    * shell-test.py: Add a third figure that directly compares the
      isotropic (dashed line) and proplyd (solid line) inner winds, for
      small values of beta (0.002 to 0.2)
    * shell-test-compare.png: And this is the figure file

--- shell-test.py
+++ shell-test.py
@@ -28,16 +28,28 @@ for innertype in "isotropic", "proplyd":
     plt.clf()
     print
 
+
+# Now combine them on the same figure
+N = 100                         # number of angles
+theta = np.linspace(0.0, np.pi, N)
+for beta, color in zip([0.002, 0.005, 0.02], "rgb"):
+    shell = Shell(beta=beta, innertype="isotropic")
     R = shell.radius(theta)
-    print R
-    # set all non-positive values to NaN to prevent plotting
     R[R <= 0.0] = np.nan
     x, y = R*np.cos(theta), R*np.sin(theta)
-    plt.plot(x, y, label="beta = {}".format(beta))
+    plt.plot(x, y, color + "--", label="isotropic: beta = {}".format(beta))
 
-plt.axis([-0.6, 0.6, -0.1, 1.1])
+    shell = Shell(beta=beta, innertype="proplyd")
+    R = shell.radius(theta)
+    R[R <= 0.0] = np.nan
+    x, y = R*np.cos(theta), R*np.sin(theta)
+    plt.plot(x, y, color, label="proplyd: beta = {}".format(beta))
+
+plt.axis([-0.2, 0.2, -0.05, 0.35])
 plt.legend()
 plt.xlabel("z")
 plt.ylabel("r")
+plt.title("Comparison between proplyd and isotropic inner winds".format(innertype))
 plt.axes().set_aspect("equal")
-plt.savefig("shell-test-isotropic.png")
+plt.savefig("shell-test-compare.png")
+plt.clf()
