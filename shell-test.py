81b9a28c00633575056c8d25e6d470a2b76c6d43
William Henney <will@henney.org>
2012-09-05 23:30:01 -0500

    Add a new figure to the bowshock shell tests that shows R90 vs R0
    This shows only a very slight difference between the isotropic and the
    proplyd cases
    
    * shell-test-R0-R90.png: The new figure
    * shell-test.py: Add graph of R90 versus R0

--- shell-test.py
+++ shell-test.py
@@ -29,10 +29,13 @@ for innertype in "isotropic", "proplyd":
     print
 
 
-# Now combine them on the same figure
+# Now combine them on the same figure for small values of eta
 N = 100                         # number of angles
 theta = np.linspace(0.0, np.pi, N)
+print
+print "Combined figure"
 for beta, color in zip([0.002, 0.005, 0.02], "rgb"):
+    print "beta = ", beta
     shell = Shell(beta=beta, innertype="isotropic")
     R = shell.radius(theta)
     R[R <= 0.0] = np.nan
@@ -53,3 +56,35 @@ plt.title("Comparison between proplyd and isotropic inner winds".format(innertyp
 plt.axes().set_aspect("equal")
 plt.savefig("shell-test-compare.png")
 plt.clf()
+
+
+# Now make a graph of R0 vs R90
+N = 10
+# This time, we can take big steps and only go up to 90 degrees
+theta = np.linspace(0.0, np.pi/2, N)
+R0 = list()                     # the on-axis radius
+R90 = list()                    # the perpendicular radius for isotropic inner wind
+R90p = list()                    # the perpendicular radius for proplyd inner wind
+beta = np.logspace(-3.0, -1.0)   # from 0.001 to 0.1 
+print
+print "R90 vs R0 figure"
+for b in beta:
+    # first do the isotropic wind
+    shell = Shell(beta=b, innertype="isotropic")
+    R = shell.radius(theta)
+    R0.append(R[0])             # first element is R0
+    R90.append(R[-1])           # last element is R90
+
+    # second do the proplyd wind
+    shell = Shell(beta=b, innertype="proplyd")
+    R = shell.radius(theta)
+    R90p.append(R[-1])           # last element is R90
+
+plt.plot(R0, R90, label="isotropic inner wind")
+plt.plot(R0, R90p, label="proplyd inner wind")
+plt.legend()
+plt.xlabel("R_0 / D")
+plt.ylabel("R_90 / D")
+plt.title("Perpendicular versus parallel bowshock radii")
+plt.savefig("shell-test-R0-R90.png")
+
