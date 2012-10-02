9cb67b377ea6d29eb18b148c5b396689c04a2ab7
Alejandro Tarango <j.tarango@crya.unam.mx>
2012-10-02 12:36:34 -0500

    Enhanced program that plots R90 vs R0. Many changes are done, including the way to compute (x,y) in the plane of sky.

--- r90-r0-rotated.py
+++ r90-r0-rotated.py
@@ -26,19 +26,18 @@ def omega(r,t):
 #parser.add_argument("--iangle", "-i", default=0.0, type=float, help="Rotation of z axis in radians")
 #cmdargs = parser.parse_args()
 #i = cmdargs.iangle
-i = np.linspace(0,45,9)
+i = np.linspace(-20,20,9)
 
 
-N = 50
-h=1e-6
+N = 100
 theta = np.linspace(0,0.5*np.pi,N)
-beta = np.linspace(0.,0.1,10)
+beta = np.linspace(0.01,0.5,20)
 innertype = ['isotropic','proplyd']
-phi = np.linspace(-np.pi,np.pi,N,endpoint = False) #Phi is the azimutal angle in spherical coordinates to make the revolution surface of proplyd
+#phi = np.linspace(-np.pi,np.pi,N,endpoint = False) #Phi is the azimutal angle in spherical coordinates to make the revolution surface of proplyd
 #Meshgrid theta and phi:
-THETA,PHI = np.meshgrid(theta,phi)
-DPHI = 2*np.pi/N
-i = np.linspace(0.0,30.0,5)
+#THETA,PHI = np.meshgrid(theta,phi)
+#DPHI = 2*np.pi/N
+#i = np.linspace(-30.0,30.0,8)
 
 for inn in innertype:
     print "******{} case******".format(inn)
@@ -48,17 +47,23 @@ for inn in innertype:
         for b in beta:
             shell = Shell(beta=b, innertype=inn)    
             R = shell.radius(theta)
+            R[R<=0] = np.nan
             w = omega(R,theta)
-            x,y,z = R*np.cos(THETA),R*np.sin(THETA)*np.cos(PHI),R*np.sin(THETA)*np.sin(PHI)
-            SenodePhiT = np.tan(j*np.pi/180.) * ( ( 1+ w*np.tan(THETA) )/( w-np.tan(THETA) ) )
+#           x,y,z = R*np.cos(THETA),R*np.sin(THETA)*np.cos(PHI),R*np.sin(THETA)*np.sin(PHI)
+            SenodePhiT = np.tan(j*np.pi/180.) * ( ( 1+ w*np.tan(theta) )/( w-np.tan(theta) ) )
             SenodePhiT[np.abs(SenodePhiT)>=1.] =np.nan #other way to set mask
-            mask = (np.sin(PHI-0.5*DPHI) <= SenodePhiT)  &  (SenodePhiT <= np.sin(PHI +0.5*DPHI))
-            xi,yi,zi = x*np.cos(j*np.pi/180.) - z*np.sin(j*np.pi/180.), y , x*np.sin(j*np.pi/180.)+z*np.cos(j*np.pi/180.) #Changed i sign
-            xim,yim = xi[mask],yi[mask] # x and y rotated and masked. 1D Arrays
-            R0.append(xim[0]) # R0 is the first element, it has only xi component 
-            R90.append(yim[-1]) # R90 is the last element, it has only yi component
+#           mask = (np.sin(PHI-0.5*DPHI) <= SenodePhiT)  &  (SenodePhiT <= np.sin(PHI +0.5*DPHI))
+#           xi,yi,zi = x*np.cos(j*np.pi/180.)-z*np.sin(j*np.pi/180.),y,x*np.sin(j*np.pi/180.)+z*np.cos(j*np.pi/180.)Convention sign in article Henney et al
+            xi,yi=R*np.cos(theta)*np.cos(j*np.pi/180.)-R*np.sin(theta)*SenodePhiT*np.sin(j*np.pi/180.),R*np.sin(theta)*np.sqrt(1-SenodePhiT**2)
+            xim,yim=xi[np.isnan(xi)==False],yi[np.isnan(yi)==False] #Removing nan elements from xi and yi
+#           xim,yim = xi[mask],yi[mask] # x and y rotated and masked. 1D Arrays
+            #print xim[0],yim[0]  Test #1
+            #print xim[-1],yim[-1] Test#2
+            R0.append(xim[0]) # Looks like is a secure criterium ( At least in the beta range (0,0.1] ). For high i and high beta (~0.5), odd things happen
+            R90.append(yim[-1]) # This is even a more secure criterium
+        #print "**********" 
         plt.plot(R0,R90,'o-',label = 'i={}'.format(j))
-    plt.legend()
+    plt.legend(loc="upper left")
     plt.axis([0,0.5,0,1.5])
     plt.xlabel("R_0 / D")
     plt.ylabel("R_90 / D")
