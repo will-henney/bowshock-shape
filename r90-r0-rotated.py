a2edc3ec1ea0a64e010ec2d9831566ce3fc5e84d
William Henney <will@henney.org>
2012-10-27 00:17:03 -0500

    Changes to Jorge's version so that it now seems to work OK, albeit a bit noisy
    * r90-r0-rotated.py: Clean up and remove the normalization of the radii.

--- r90-r0-rotated.py
+++ r90-r0-rotated.py
@@ -29,8 +29,14 @@ def R90_finder(x,y):
     """
     size = np.size(x)
     sizey = np.size(y)
-    if size != sizey:
-       return KeyError
+    assert size == sizey, "x and y array sizes must be equal" 
+
+    # # If inclination is < 90, then x decreases monotonically
+    # npos = len(x[x > 0.0])         # number of positive x values
+    # if npos > 0 and npos > size:
+    #     out = 0.5*(y[npos-1]+y[npos])
+    #     return out
+
     for i in range(0,size-1):
         s = x[i]*x[i+1]
         if s<0:
@@ -47,43 +53,51 @@ def R90_finder(x,y):
 #i = cmdargs.iangle
 
 beta = [0.3, 0.1, 0.01, 0.001, 1.e-4, 1.e-5, 1.e-7]
-N = 200
+Nth = 200
+Ninc = 50
 #beta = np.linspace(0.01,0.5,20)
-innertype = ['isotropic','proplyd']
-#phi = np.linspace(-np.pi,np.pi,N,endpoint = False) #Phi is the azimutal angle in spherical coordinates to make the revolution surface of proplyd
-#Meshgrid theta and phi:
-#THETA,PHI = np.meshgrid(theta,phi)
-#DPHI = 2*np.pi/N
-#i = np.linspace(-30.0,30.0,8)
 
-#for inn in innertype:
-#print "******{} case******".format(inn)
-for b in beta:
-    thlim = theta_lim(b)
-    theta = np.linspace(0,thlim,N)
-    i = np.linspace(-0.98*(thlim - 0.5*np.pi),0.98*(thlim - 0.5*np.pi),N)
-    R0 = list()
-    R90 = list()
-    shell = Shell(beta=b, innertype='isotropic')    
-    R = shell.radius(theta)
-    R*= (1+np.sqrt(b))/np.sqrt(b)
-    R[R<=0] = np.nan
-    w = omega(R,theta)
-    for j in i:
-#       x,y,z = R*np.cos(THETA),R*np.sin(THETA)*np.cos(PHI),R*np.sin(THETA)*np.sin(PHI)
-        SenodePhiT = np.tan(j) * ( ( 1+ w*np.tan(theta) )/( w-np.tan(theta) ) )
-        SenodePhiT[np.abs(SenodePhiT)>=1.] =np.nan #other way to set mask
-#       mask = (np.sin(PHI-0.5*DPHI) <= SenodePhiT)  &  (SenodePhiT <= np.sin(PHI +0.5*DPHI))
-#       xi,yi,zi = x*np.cos(j*np.pi/180.)-z*np.sin(j*np.pi/180.),y,x*np.sin(j*np.pi/180.)+z*np.cos(j*np.pi/180.)Convention sign in article Henney et al
-        xi,yi=R*np.cos(theta)*np.cos(j)-R*np.sin(theta)*SenodePhiT*np.sin(j),R*np.sin(theta)*np.sqrt(1-SenodePhiT**2)
-        xim,yim=xi[np.isnan(xi)==False],yi[np.isnan(yi)==False] #Removing nan elements from xi and yi
-        R0.append(xim[0]) # Looks like is a secure criterium ( At least in the beta range (0,0.1] ). For high i and high beta (~0.5), odd things happen
-        R90.append(R90_finder(xim,yim))     
-    plt.plot(R0,R90,'o-',label = 'beta={}'.format(b))
-plt.legend()
+innertype = ['isotropic','proplyd']
+# innertype = ['isotropic']
+
+
+ls = dict(isotropic = '-', proplyd = '--')
+for inn in innertype:
+    print "******{} case******".format(inn)
+    for b in beta:
+        print "beta = ", b
+        thlim = theta_lim(b)
+        theta = np.linspace(0,thlim,Nth)
+        i = np.linspace(-0.98*(thlim - 0.5*np.pi),0.98*(thlim - 0.5*np.pi),Ninc)
+        R0 = list()
+        R90 = list()
+        shell = Shell(beta=b, innertype=inn)    
+        R = shell.radius(theta)
+        w = omega(R,theta)
+        for j in i:
+    #       x,y,z = R*np.cos(THETA),R*np.sin(THETA)*np.cos(PHI),R*np.sin(THETA)*np.sin(PHI)
+            SenodePhiT = np.tan(j) * ( ( 1+ w*np.tan(theta) )/( w-np.tan(theta) ) )
+            SenodePhiT[np.abs(SenodePhiT)>=1.] =np.nan #other way to set mask
+    #       mask = (np.sin(PHI-0.5*DPHI) <= SenodePhiT)  &  (SenodePhiT <= np.sin(PHI +0.5*DPHI))
+    #       xi,yi,zi = x*np.cos(j*np.pi/180.)-z*np.sin(j*np.pi/180.),y,x*np.sin(j*np.pi/180.)+z*np.cos(j*np.pi/180.)Convention sign in article Henney et al
+            xi = R*np.cos(theta)*np.cos(j)-R*np.sin(theta)*SenodePhiT*np.sin(j)
+            yi = R*np.sin(theta)*np.sqrt(1-SenodePhiT**2)
+            mask = np.isfinite(xi) & np.isfinite(yi)
+            xim, yim=xi[mask], yi[mask] #Removing nan elements from xi and yi
+            R0.append(xim[0]) # Looks like is a secure criterium ( At least in the beta range (0,0.1] ). For high i and high beta (~0.5), odd things happen
+            R90.append(R90_finder(xim,yim))
+        if inn == "isotropic":
+            label = 'beta={}'.format(b)
+        else:
+            label = None
+        plt.plot(R0,R90, ls[inn], label = label)
+
+
+plt.legend(loc="upper left")
 #    plt.axis([0,0.5,0,1.5])
 plt.xlabel("R_0 / D")
+plt.ylim(0.0, 4.0)
 plt.ylabel("R_90 / D")
 plt.title("Perpendicular versus parallel bowshock radii")
-plt.savefig("{}-shell-test2-R0-R90.png".format('isotropic'))
+plt.savefig("combined-shell-test2-R0-R90.pdf")
 plt.clf()
