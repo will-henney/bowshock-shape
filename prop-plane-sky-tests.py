abe240b2c2214c64757ea2e9e609d335a6673cdd
Alejandro Tarango <j.tarango@crya.unam.mx>
2012-11-13 12:19:25 -0600

    Performed program for bowshocks shapes. Needs cosmetic details but roughly can be seen the issues of bowshocks at high beta and inclinations. There is a notorious "knee" in some bowshocks. The outputs are saved in Dropbox

--- prop-plane-sky-tests.py
+++ prop-plane-sky-tests.py
@@ -2,9 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from equation6 import Shell
 import argparse
+from scipy.optimize import fsolve,bisect
 """
 In this script, we'll create the proyection of proplyd in the sky plane, assuming the plane of proplyd is rotated respect plane of sky by an angle i
 """
+
+def theta_lim(beta): 
+    "Asymptotic opening angle from CRW eq (28)"
+    def f(theta):
+	"Function to be zeroed: f(theta) = 0 for theta = theta_lim"
+	return theta - np.tan(theta) - np.pi/(1.0 - beta)
+    return bisect(f, 0.5*np.pi+0.01, np.pi)
 
 def OMEGA(r,rpdr,h):
 
@@ -21,45 +29,41 @@ def omega(r,t):
     womega[:-1] = np.diff(np.log(r))/np.diff(theta)
     return womega
 
-#First, design a set of beta values to calculate R(theta) (Well, first than that, desgign a theta array)
-#parser = argparse.ArgumentParser(description="Rotation angle")
-#parser.add_argument("--iangle", "-i", default=0.0, type=float, help="Rotation of z axis in radians")
-#cmdargs = parser.parse_args()
-#i = cmdargs.iangle
+Nth = 200
+Ninc = 10
 
-N = 200
-#h=1e-6
-theta = np.linspace(0,np.pi,N)
-beta = np.array([0.15, 0.05, 0.02, 0.005,0.2,0.3])
+beta = np.array([0.16, 0.08, 0.04, 0.02, 0.01])
 innertype = ['isotropic','proplyd']
-#phi = np.linspace(-np.pi,np.pi,N,endpoint = False) #Phi is the azimutal angle in spherical coordinates to make the revolution surface of proplyd
-#Meshgrid theta and phi:
-#THETA,PHI = np.meshgrid(theta,phi)
-#DPHI = 2*np.pi/N
-i = np.linspace(0.0,0.5236,8)
-for inn in innertype:
-    print "******{} case******".format(inn)
-    for b in beta:
-        for j in i:
-            shell = Shell(beta=b,innertype=inn)
-#           Calculing R(theta)
-            R=shell.radius(theta)
-#           Rdr = shell.radius(theta + h)
-            R[R<=0] = np.nan # Set neagtive R to NaN
-#           W = OMEGA(R,Rdr,h)
-            w = omega(R,theta)
-#           x,y,z = R*np.cos(THETA),R*np.sin(THETA)*np.cos(PHI),R*np.sin(THETA)*np.sin(PHI)
-            SenodePhiT = np.tan(j) * ( ( 1+ w*np.tan(theta) )/( w-np.tan(theta) ) )
- #          mask = np.abs(SenodePhiT) <= 1.  #Discard non real values for the angle of tangent line
-            SenodePhiT[np.abs(SenodePhiT)>=1.] =np.nan #other way to set mask
-#           mask = (np.sin(PHI-0.5*DPHI) <= SenodePhiT)  &  (SenodePhiT <= np.sin(PHI +0.5*DPHI))
-            xi,yi=R*np.cos(theta)*np.cos(j)-R*np.sin(theta)*SenodePhiT*np.sin(j),R*np.sin(theta)*np.sqrt(1-SenodePhiT**2) #Uses eq. A9 from Henney et al 
-#                                                                           instead eq A1, A2. The mask in this case 
-            plt.plot(xi,yi,label= 'i={}'.format(j))                             #becomes unnecessary
-        plt.axis([-1.0,1.0,-0.05,2.0])
-        plt.legend()
-        plt.xlabel('z')
-        plt.ylabel('r')
-        plt.title('Bowshock shapes for {} wind and beta = {}'.format(inn,b))
-        plt.savefig('{}-wind-plane-sky-beta-{}.png'.format(inn,b))
-        plt.clf()
+lw = dict(isotropic = 1, proplyd = 2)
+for b in beta:
+    print "******beta = {}******".format(b)
+    thlim = theta_lim(b) 
+    theta = np.linspace(0,thlim,Nth)
+    for inn in innertype:
+        if inn == 'proplyd':
+            inc = np.linspace(0.0, 0.5*np.pi, Ninc)   
+        else:
+            inc = np.linspace(0.,0.98*(thlim - 0.5*np.pi),Ninc)
+        print "Maximum Inclination: ", np.degrees(inc[-1])
+        shell = Shell(beta=b,innertype=inn)
+#       Calculing R(theta)
+        R=shell.radius(theta)
+        R[R<=0] = np.nan # Set neagtive R to NaN
+        w = omega(R,theta)
+        for jnc in inc:
+            SenodePhiT = np.tan(jnc) * ( ( 1+ w*np.tan(theta) )/( w-np.tan(theta) ) )
+            SenodePhiT[np.abs(SenodePhiT)>=1.] =np.nan 
+            xi = R*np.cos(theta)*np.cos(jnc)-R*np.sin(theta)*SenodePhiT*np.sin(jnc)
+            yi = R*np.sin(theta)*np.sqrt(1-SenodePhiT**2) 
+            if inn == "isotropic":
+                label = 'i={}'.format(jnc)
+            else:
+                label = None
+            plt.plot(xi,yi,linewidth=lw[inn],label=label)                             
+#    plt.axis([-1.0,1.0,-0.05,yi[-1]+0.5])
+    plt.legend()
+    plt.xlabel('z')
+    plt.ylabel('r')
+    plt.title('Bowshock shapes for wind and beta = {}'.format(b))
+    plt.savefig('bowshock-wind-plane-sky-beta-{}.pdf'.format(b))
+    plt.clf()
