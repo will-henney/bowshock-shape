Program for plotting bowshock shapes. Bug in the graph corrected. Also displays the minimal radius, taken as R0. And the average radius for the points with largest theta (in absolute magnitude). This is Rmax

read-shapes/read-region-file.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-regionfile = "LV-OIII-positions-2.reg" 
+regionfile = "LV-OIII-positions-3.reg" 
 
 def extract_2nd(x):
     """
@@ -119,11 +119,12 @@ for proplyd in proplyds:
     th2nda,th2nd = extract_2nd(theta)
     th3rda,th3rd = extract_2nd(th2nda)
     theta_mean,rt = extract_n_mean(theta,R,th3rd)
+    plt.plot(x, y, "o", label="{}: D = {:.2f} arcsec".format(proplyd, D))
     for x,y in zip(R,theta):
         if x == np.array(R).min():
             r0,th0 = x,y            
     print "{} {:.2f} {:.2f} {:.2f} {:.2f}".format(proplyd,r0,th0,rt,theta_mean)
-    plt.plot(x, y, "o", label="{}: D = {:.2f} arcsec".format(proplyd, D))
+    
 
 plt.plot(0.0, 0.0, "rx", label=None)
 plt.legend(loc="lower left", prop=dict(size="x-small"))