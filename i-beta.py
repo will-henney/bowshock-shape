3ed9d0c2acf03d9bff804f9b0e7d94da8cf3b758
Alejandro Tarango <j.tarango@crya.unam.mx>
2013-08-14 17:02:41 -0500

    Plot beta vs i (in construction)

---
+++ i-beta.py
@@ -0,0 +1,45 @@
+"""
+La idea es crear un script que tenga una funcion que me de Rc'\D' y R0'/D' 
+dandole como entrada beta, para toda i posible. Luego, dar un intervalo para 
+Rc/Ro y para R0/D y de la salida que me dio ver cuales valores de i dan un R0 
+y Rc que caigan en el intervalo, y guardar esos valores de i en un archivo, y 
+graficar despues  
+"""
+#import libprop
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+from scipy.optimize import bisect, leastsq
+from equation6 import Shell
+import json
+"""
+* First step:  Set interval for R0 and Rc
+* Second step: Create the bowshock NA
+* Third step:  Measure Ro and Rc NA
+* 2nd A step: Read the file with de R0-Rc curves
+Repeat second A -and third step- for all possible inclinations (End of first test)
+* Fourth step: Check if R0 and Rc fits into the interval (Possible results: Yes or No)
+* Fifth step: if forurth is true, graph the corresponding b & i, if false, don't do anything
+* Sixth step: Repeat from second to fifth step for many i's and b's
+"""
+
+"""
+set interval:
+For LV3, for example:
+R0/D = 0.336 +- 1e-3
+Rc/R0 = 2.0342262 +- 0.139
+"""
+
+R0m   = 0.336
+R0d   = 1e-3
+xlow  = R0m-R0d
+xhigh = R0m+R0d
+
+Rcm   = 2.0342262
+Rcd   = 0.139
+ylow  = Rcm-Rcd  
+yhigh = Rcm+Rcd
+
+inc  = np.linspace(0,90,200)
+incr = np.radians(inc)
+
