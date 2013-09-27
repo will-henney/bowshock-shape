1410fe51690855763bc4c8fe56b230a29d6c1df3
William Henney <will@henney.org>
2013-09-27 13:41:30 -0500

    Fit analytic function to reciprocal of Rc/R0 vs beta

--- AvsBeta.py
+++ AvsBeta.py
@@ -11,8 +11,24 @@ for modeldata in shelldata.items():
     r0 = np.array(param["R0'"])
     rc = np.array(param["Rc"])
     inc = np.array(param["inc"])
-    plt.plot(np.array(float(beta)), rc[inc==0.0]/r0[inc==0.0], "r.")
+    plt.plot(np.array(float(beta)), r0[inc==0.0]/rc[inc==0.0], "r.")
+
+def y(x, b, d): 
+    c = np.exp(-1.0/b)
+    return y0*(np.exp(-((x**d)/b)) - c)/(1.0 - c)
+
+y0 = 0.66
+b = 50.0
+d = 0.49
+x = np.linspace(0.0, 0.1, 500)
+plt.plot(x, y(x, b, d))
 plt.grid()
 plt.xlabel("beta")
-plt.ylabel("A")
+plt.ylabel("1/A")
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
 plt.savefig("AVSb.pdf")
+print "b = ", b
+print "d = ", d
+print "exp(-1/b) = ", np.exp(-1.0/b)
+print "y0^-1 = ", 1./y0
