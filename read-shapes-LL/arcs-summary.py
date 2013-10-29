bfe3b22b5b276a9f6ab3f189d1fe0eb34af9420e
William Henney <will@henney.org>
2013-10-29 09:03:55 -0600

    Restore previous arcs-summary/py from Dropbox
    It turns out that I had over-written Luis Ángel's version of this file.
    Thankfully, Dropbox keeps the previous 30 days of changes and allows you
    to restore!

--- read-shapes-LL/arcs-summary.py
+++ read-shapes-LL/arcs-summary.py
@@ -20,6 +20,12 @@ def write_table(columns, col_names):
 id_ = []
 ra = []
 dec = []
+d = []
+pa_star = []
+pa_out = []
+pa_in = []
+r_out = []
+r_in = []
 
 for file_name in file_list:
     with open(file_name) as f:
@@ -29,9 +35,21 @@ for file_name in file_list:
     id_.append(data["star"]["id"])
     ra.append(data["star"]["RA"])
     dec.append(data["star"]["Dec"])
-
+    d.append(str(data["star"]["D"]))
+    pa_star.append(str(data["star"]["PA"]))
+    pa_out.append(str(data["outer"]["PA0"]))
+    try:
+        pa_in.append(str(data["inner"]["PA0"]))
+    except:
+        pa_in.append("-"+"\t")
+    r_out.append(str(data["outer"]["R0"]))
+    try:
+        r_in.append(str(data["inner"]["R0"]))
+    except:
+        r_in.append("-"+"\t")
+     
     # D = ???
-    # PA_star = 
+    # PA_star =
     # PA_in = 
     # PA_out = 
     # R_in = 
@@ -41,7 +59,7 @@ for file_name in file_list:
 # Write output table to a file
 with open("arcs-summary.tab", "w") as f:
     f.write(write_table( 
-        [id_, ra, dec], 
-        ["Object", "RA", "Dec"]
+        [id_, ra, dec, d, pa_star, pa_out, pa_in, r_out, r_in], 
+        ["Object", "RA", "Dec", "D", "PA_star", "PA_out", "PA_in", "R_out", "R_in"]
     ))
     
