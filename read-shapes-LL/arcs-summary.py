08da9815433a360687c2f0022f632b15f449967a
William Henney <will@henney.org>
2013-10-29 22:35:00 -0600

    Refactor summary table program and add columns
    Now include radii of curvature

--- read-shapes-LL/arcs-summary.py
+++ read-shapes-LL/arcs-summary.py
@@ -1,7 +1,7 @@
 import glob
 import json
 
-pattern = "j8oc??010_drz/*-xy.json"
+pattern = "j8oc??010_drz/*-xyc.json"
 
 file_list = glob.glob(pattern)
 
@@ -17,36 +17,48 @@ def write_table(columns, col_names):
         
 
 # Initialize a list for each column in output table
-id_ = []
-ra = []
-dec = []
-d = []
-pa_star = []
-pa_out = []
-pa_in = []
-r_out = []
-r_in = []
+col_names = ["Object", "RA", "Dec", "D", "PA_star", "PA_out", "PA_in", "R_out", "R_in", "Rc_out", "Rc_in"]
+table = {cn: [] for cn in col_names}
+
+
+def PA_fmt(pa):
+    """Write position angles to accuracy of 0.1 deg"""
+    return "{:.1f}".format(pa)
+
+
+def arcsec_fmt(r):
+    """ Write distances to accuracy of 0.001 arcsec"""
+    return "{:.3f}".format(r)
+
 
 for file_name in file_list:
     with open(file_name) as f:
         data = json.load(f)
 
     # Add this object's data to each output column
-    id_.append(data["star"]["id"])
-    ra.append(data["star"]["RA"])
-    dec.append(data["star"]["Dec"])
-    d.append(str(data["star"]["D"]))
-    pa_star.append(str(data["star"]["PA"]))
-    pa_out.append(str(data["outer"]["PA0"]))
-    try:
-        pa_in.append(str(data["inner"]["PA0"]))
-    except:
-        pa_in.append("-"+"\t")
-    r_out.append(str(data["outer"]["R0"]))
-    try:
-        r_in.append(str(data["inner"]["R0"]))
-    except:
-        r_in.append("-"+"\t")
+    table["Object"].append(data["star"]["id"])
+    table["RA"].append(data["star"]["RA"])
+    table["Dec"].append(data["star"]["Dec"])
+    table["D"].append(arcsec_fmt(data["star"]["D"]))
+    table["PA_star"].append(PA_fmt(data["star"]["PA"]))
+    if "outer" in data:
+        table["PA_out"].append(PA_fmt(data["outer"]["PA0"]))
+        table["R_out"].append(arcsec_fmt(data["outer"]["R0"]))
+        table["Rc_out"].append(arcsec_fmt(data["outer"]["Rc"]))
+    else:
+        table["PA_out"].append("-")
+        table["R_out"].append("-")
+        table["Rc_out"].append("-")
+        
+    if "inner" in data:
+        table["PA_in"].append(PA_fmt(data["inner"]["PA0"]))
+        table["R_in"].append(arcsec_fmt(data["inner"]["R0"]))
+        table["Rc_in"].append(arcsec_fmt(data["inner"]["Rc"]))
+    else:
+        table["PA_in"].append("-")
+        table["R_in"].append("-")
+        table["Rc_in"].append("-")
+        
      
     # D = ???
     # PA_star =
@@ -58,8 +70,5 @@ for file_name in file_list:
  
 # Write output table to a file
 with open("arcs-summary.tab", "w") as f:
-    f.write(write_table( 
-        [id_, ra, dec, d, pa_star, pa_out, pa_in, r_out, r_in], 
-        ["Object", "RA", "Dec", "D", "PA_star", "PA_out", "PA_in", "R_out", "R_in"]
-    ))
+    f.write(write_table([table[cn] for cn in col_names], col_names))
     
