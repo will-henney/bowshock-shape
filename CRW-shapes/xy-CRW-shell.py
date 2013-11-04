2dac8b322416957410b6f47021c6ed3616bd735a
Alejandro Tarango <j.tarango@crya.unam.mx>
2013-11-04 17:03:42 -0600

    performed to use any beta value
    it still needs to be performed to include rotations

--- CRW-shapes/xy-CRW-shell.py
+++ CRW-shapes/xy-CRW-shell.py
@@ -7,26 +7,31 @@ import argparse
 import json
 from misc_utils import run_info
 """
-Goal: The idea of this program is create a CRW bowshock and write an output *-forma.reg file to use it later with Will's former programs
+Goal: The idea of this program is create a CRW bowshock and write an output *xy.json file to use it later with Will's former programs
 Steps: 
 1.- Create the bowshock arc with the equation6 library for a fixed beta parameter for isotropic and/or proplyd case (perform to any beta value for both 
 cases).
-2.- Assign a fictional RA-Dec proplyd position (maybe the same position of a known proplyd)
+
 3.- Use the astropy packages as well other libraries to write the coordinates of the arc in a ds9 region format  
 """
+parser = argparse.ArgumentParser(description = "model parameters")
+parser.add_argument("--beta",type= float,default=0.01,help="two winds momentum ratio")
+parser.add_argument("--inclination",type=float,default=0.0,help="inlination angle")
+parser.add_argument("--type",type=str,default="isotropic",help="type of inner wind")
+cmd_args = parser.parse_args()
 
 #1
-beta = 0.01
+beta = cmd_args.beta
 theta = np.linspace(0,0.5*np.pi,500)
-shell = Shell(beta,"isotropic")
+shell = Shell(beta,cmd_args.type)
 R = shell.radius(theta)
-
+i=cmd_args.inclination
 arcdata = {
     "info":{
         "Description": "CRW theoretical stationary bowshocks",
         "history":["Initially created by "+ run_info()],
         "beta": beta,
-        "i": 0.0,
+        "i": i,
         "inner wind type": "isotropic"
         },
     "outer":{
@@ -36,14 +41,14 @@ arcdata = {
     "help":{
         "beta":"Two-winds momentum ratio",
         "i": "Rotation angle between proplyd reference frame and observer reference frame",
-        "inner wind  type": "Angular variation of density's inner wind",
-        "x": "(list) x-coordinates of the outer shell",
-        "y": "(list) y-coordinates of the outer shell",
+        "inner wind type": "Angular variation of density's inner wind",
+        "x": "(list) x-coordinates of the outer shell normalized with distance",
+        "y": "(list) y-coordinates of the outer shell normalized with distance"
         }
 
 }
 
-jsonfile= "teo-shell-beta-{}.json".format(beta)
+jsonfile= "teo-shell-beta-{}-i-{}.json".format(beta,i)
 with open(jsonfile,"w") as f:
     json.dump(arcdata,f,indent=4)
 
