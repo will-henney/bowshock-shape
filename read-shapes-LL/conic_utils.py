726490adaf47325101021ff096fd620d6df914c6
William Henney <will@henney.org>
2013-11-06 13:50:20 -0600

    Improve conic section fits

--- read-shapes-LL/conic_utils.py
+++ read-shapes-LL/conic_utils.py
@@ -174,7 +174,9 @@ if __name__ == "__main__":
 
     datadir = os.path.expanduser("~/Dropbox/JorgeBowshocks/")
     datafiles = glob.glob(datadir + "*/*/*-xyc.json")
-    colors = "kkkbgrcmy"
+    thetas = [75.0, 60.0, 45.0, 30.0, 15.0, 0.0001, -15.0, -30.0, -45.0, -60.0, -75.0, -15.0]
+    lws = [1.0]*5 + [1.0] + [0.5]*5 + [2.0]
+    colors = "ymcrgbgrcmyk"
     for datafile in datafiles:
         print "*"*80
         print "Processing ", datafile
@@ -183,13 +185,15 @@ if __name__ == "__main__":
         testdata = db["outer"]
 
         fig = plt.figure()
-        ax = fig.add_subplot(311)
+        ax = fig.add_subplot(221)
         ax.plot(testdata['x'], testdata['y'], 'ko', alpha=0.4)
-        axres = fig.add_subplot(312)
-        axsig = fig.add_subplot(313)
+        axres = fig.add_subplot(222)
+        axsig = fig.add_subplot(223)
+        axr90 = fig.add_subplot(224)
         model_theta_infs = []
         model_sigmas = []
-        for theta_inf, color in zip([45.0, 30.0, 15.0, -0.0001, -15.0, -30.0, -45.0, -60.0, -15.0], colors):
+        model_R90s = []
+        for theta_inf, color, lw in zip(thetas, colors, lws):
             Rh, thh, PAh, xxh, yyh, fit = fit_conic(
                 xx=np.array(testdata["x"]),
                 yy=np.array(testdata["y"]),
@@ -198,20 +202,26 @@ if __name__ == "__main__":
                 PAh=testdata["PAc"],
                 xxh=testdata["xc"],
                 yyh=testdata["yc"],
-                freeze_theta=color!="y",
+                freeze_theta=not lw==2.0,
                 full=True,
                 Rmin=testdata["R0"],
             )
             x, y = world_hyperbola(Rh, thh, PAh, xxh, yyh)
-            label = r"$\theta_{{\infty}} = {}^\circ$, $R_{{\mathrm{{c}}}} = {:.2f}''$".format(int(thh), Rh)
-            ax.plot(x, y, '-' + color, label=label, alpha=0.5)
-            ax.arrow(xxh, yyh, 2*Rh*np.sin(np.radians(PAh)), 2*Rh*np.cos(np.radians(PAh)), 
-                     fc='none', ec=color, width=0.0003, alpha=0.5, lw=0.5, head_width=0.05*Rh, head_length=0.1*Rh)
-            ax.plot([xxh], [yyh], '.' + color, alpha=0.5)
-            axres.plot(fit['x']*Rh, fit['residual'], ls='-', color=color, marker='.', label=label, alpha=0.5)
+            R90 = Rh*x90(testdata["R0"]/Rh, thh)
+            model_R90s.append(R90)
             model_theta_infs.append(thh)
             rms = np.sqrt(np.mean(fit['residual']**2))
             model_sigmas.append(rms)
+            try:
+                label = r"$\theta_{{\infty}} = {}^\circ$, $R_{{\mathrm{{90}}}} = {:.2f}''$".format(int(thh), R90)
+                ax.plot(x, y, '-' + color, label=label, alpha=0.5, lw=lw)
+                ax.arrow(xxh, yyh, 2*Rh*np.sin(np.radians(PAh)), 2*Rh*np.cos(np.radians(PAh)), 
+                         fc='none', ec=color, width=0.0003, alpha=0.5, lw=0.5, head_width=0.05*Rh, head_length=0.1*Rh)
+                ax.plot([xxh], [yyh], '.' + color, alpha=0.5)
+                axres.plot(fit['x']*Rh, fit['residual'], ls='-', color=color, marker='.', label=label, alpha=0.5, lw=lw)
+            except ValueError:
+                label = "No solution"
+                
 
         ax.axis('equal')
         scale = testdata["Rc"]
@@ -227,18 +237,31 @@ if __name__ == "__main__":
         axres.set_xlabel("Lateral coordinate, x, arcsec")
         axres.set_ylabel("(Model - data) for axial coordinate, y, arcsec")
         axres.set_xlim(None, None)
-        res_scale = 5*np.array(model_sigmas).min()
+        # res_scale = 2*np.array(model_sigmas).mean()
+        res_scale = testdata["R0"]
         axres.set_ylim(-res_scale, res_scale)
         axres.grid(alpha=0.2, linestyle='-')
 
-        axsig.scatter(model_theta_infs, model_sigmas, c=colors)
+        axsig.plot(model_theta_infs[:-1], model_sigmas[:-1], "k-")
+        for th, sig, c in zip(model_theta_infs, model_sigmas, colors):
+            axsig.plot(th, sig, color=c, marker="o")
         axsig.set_xlabel("theta_inf, degrees")
         axsig.set_ylabel("RMS residual, arcsec")
         axsig.set_xlim(-90.0, 90.0)
         axsig.set_ylim(0.0, None)
         axsig.grid(alpha=0.2, linestyle='-')
 
-        fig.set_size_inches(6, 15)
+        axr90.plot(model_theta_infs[:-1], model_R90s[:-1], "k-")
+        for th, R90, c in zip(model_theta_infs, model_R90s, colors):
+            axr90.plot(th, R90, color=c, marker="o")
+        axr90.plot([-90.0, 90.0], [testdata["R0"], testdata["R0"]], "k-", lw=4.0, alpha=0.3)
+        axr90.set_xlabel("theta_inf, degrees")
+        axr90.set_ylabel("R90, arcsec")
+        axr90.set_xlim(-90.0, 90.0)
+        axr90.set_ylim(0.0, None)
+        axr90.grid(alpha=0.2, linestyle='-')
+
+        fig.set_size_inches(12, 12)
         fig.tight_layout()
         figfile = os.path.basename(datafile).replace("-xyc.json", "-hyper-test.png")
         fig.savefig("test/" + figfile, dpi=300)
