a5fd52ef2d051c68efed3aef226c03ff379bf6fd
William Henney <will@henney.org>
2013-11-25 11:50:37 -0600

    First stab at identifying dataset from image names
    This uses the parse package.  It does work, but I may refactor to used
    named groups in regexps to avoid the dependency, since parse is not in
    the standard library.

--- read-shapes-LL/misc_utils.py
+++ read-shapes-LL/misc_utils.py
@@ -3,6 +3,7 @@ import getpass
 import sys
 import os
 import json
+import parse
 
 def run_info():
     """
@@ -32,14 +33,9 @@ def update_json_file(data, jsonfile):
 
 
 def short_image_name(long_name):
-    if "hlsp_orion_hst_" in long_name:
-        short_name = long_name.replace("hlsp_orion_hst_", 
-                                       "Robberto_").replace("_v1_drz", "")
-    elif long_name.startswith("j8oc"):
-        short_name = long_name.replace("j8oc", "Bally_").replace("010_wcs", "")
-    else:
-        short_name = long_name
-    return short_name
+    imset, imset_data = find_image_set(long_name)
+    return "_".join([imset.replace(" ", "_")] + [str(v) for v in imset_data.values ()])
+
 
 
 def who_am_i():
@@ -64,6 +60,44 @@ def who_am_i():
     return "{}@{}".format(user, system)
 
 
+IMAGE_SET_PATTERNS = {
+    "Bally": "j8oc{field}010_wcs", 
+    "Robberto ACS": "hlsp_orion_hst_acs_strip{field}_{filter}_v1_drz",
+    "Robberto WFPC2": "hlsp_orion_hst_wfpc2_{field}_{filter}_v1_sci",
+    "PC": "{field:d}f{filter:3w}",
+    "PC mosaic": "wcs_GO5469PC{filter}e",
+    "WFC mosaic": "mosaic{filter}",
+}
+UNKNOWN_SET_PATTERNS = {
+    "Unknown dataset": "f{filter:3w}",
+}
+
+RE_FILTER_NAME = "f(?P<filter>\d\d\d)"
+
+def find_image_set(filename):
+    """Auto detect which dataset a given file is from
+    
+    Returns name of image set and dict containing other info gleaned
+    from the filename, such as field id and filter name.
+
+    Uses the parse module, which is a little nicer than regexps,
+    although maybe I should just learn to use named groups: 
+    (?P<name>...), then accesed via match.groupdict()
+
+    """
+    for imset, pattern in IMAGE_SET_PATTERNS.items():
+        match = parse.search(pattern, filename)
+        if match:
+            return imset, match.named
+    else:
+        for imset, pattern in UNKNOWN_SET_PATTERNS.items():
+            match = parse.search(pattern, filename)
+            if match:
+                return imset, match.named
+        else:
+            return "Unknown dataset no filter", {}
+    
+
 def fits_dirs():
     """
     Find the root directory for various collections of FITS files
@@ -76,7 +110,8 @@ def fits_dirs():
         if user == "will@mac":
             large_fits_dir = "/Users/will/OrionTreasury"
         else:
-            raise(EnvironmentError, "Unrecognised user@system - please add to misc_utils.py")
+            raise(EnvironmentError, 
+                  "Unrecognised user@system - please add to misc_utils.py")
 
     if user.startswith("will"):
         dropbox_folder = "JorgeBowshocks"
@@ -84,12 +119,14 @@ def fits_dirs():
         dropbox_folder = "ProplydMIR"
 
     small_fits_dir = os.path.join(dropbox_root, dropbox_folder, "HST")
-    return {
-        "Bally": os.path.join(large_fits_dir, "BallyACS"), 
-        "Robberto ACS": os.path.join(large_fits_dir, "acs"),
-        "Robberto WFPC2": os.path.join(large_fits_dir, "wfc"),
-        "PC": small_fits_dir,
-        "PC mosaic": small_fits_dir,
-        "WFC mosaic": small_fits_dir
-    }
+    return small_fits_dir, large_fits_dir
+
+    # return {
+    #     "Bally": os.path.join(large_fits_dir, "BallyACS"), 
+    #     "Robberto ACS": os.path.join(large_fits_dir, "acs"),
+    #     "Robberto WFPC2": os.path.join(large_fits_dir, "wfc"),
+    #     "PC": small_fits_dir,
+    #     "PC mosaic": small_fits_dir,
+    #     "WFC mosaic": small_fits_dir
+    # }
 
