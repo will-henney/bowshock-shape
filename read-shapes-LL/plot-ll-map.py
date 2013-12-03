aedfc7db738479747086fca1b1725ebb8ef900e8
William Henney <will@henney.org>
2013-12-03 15:30:25 -0600

    Make arrows larger

--- read-shapes-LL/plot-ll-map.py
+++ read-shapes-LL/plot-ll-map.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from astropysics import coords
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,7 +15,8 @@ def find(name, path):
     return None
             
 
-def plot_map(limits, figname, canvas_size, exclude=0.0):
+def plot_map(limits, figname, canvas_size, exclude=0.0, arrowscale=1.0):
+    plt.clf()
     x, y = coords.radec_str_to_decimal(RAs, Decs) 
     x = -(x - x0)*3600.0
     y = (y - y0)*3600.0
@@ -36,24 +38,17 @@ def plot_map(limits, figname, canvas_size, exclude=0.0):
         # Try and draw the inner and outer arcs
         #
 
-        # First, work out where the JSON file is...
-        try:
-            field = "{:02d}".format(field_list)
-        except:
-            try:
-                field = field_list[:2]
-            except:
-                print "failed"
-                continue
-
-        # folder = "j8oc{}010_drz".format(field)
         jsonfile = "{}-arcdata.json".format(label.split()[-1])
-        found = find(jsonfile, "..")
+        found = find(jsonfile, "../Jorge_prop/PC-correct")
         if found is not None:
             f = open(found)
         else:
-            print "Could not open ", jsonfile
-            continue
+            found = find(jsonfile, ".")
+            if found is not None:
+                f = open(found)
+            else:
+                print("Could not open ", jsonfile)
+                continue
 
         # Second, load in the data and draw the arcs
         arc_data = json.load(f)
@@ -62,16 +57,17 @@ def plot_map(limits, figname, canvas_size, exclude=0.0):
                 dx = np.array(arc_data[arc]["x"])
                 dy = np.array(arc_data[arc]["y"])
                 plt.plot(xx - dx, yy + dy, "-" + color, lw=1.0, alpha=0.6)
-                print "Plotted {} arc for {}".format(arc, found)
+                print("Plotted {} arc for {}".format(arc, found))
                 # Now try and draw arrows for the arc axes too
-                R0 = arc_data[arc]["R0"]
-                PA = np.radians(arc_data[arc]["PA0"])
-                ax = -R0*np.sin(PA)
-                ay = R0*np.cos(PA)
-                plt.arrow(xx, yy, 4*ax, 4*ay, fc='none', ec=color, 
-                          width=0.0003, alpha=0.6, lw=0.5,
-                          head_width=2.0, head_length=4.0,
-                )
+                # R0 = arc_data[arc]["R0"]
+                # PA = np.radians(arc_data[arc]["PA0"])
+                # ax = -R0*np.sin(PA)
+                # ay = R0*np.cos(PA)
+                # plt.arrow(xx, yy, 4*ax*arrowscale, 4*ay*arrowscale,
+                #           fc='none', ec=color, 
+                #           width=0.0006, alpha=0.6, lw=0.5,
+                #           head_width=4.0*arrowscale, head_length=8.0*arrowscale,
+                # )
                 if "Rc" in arc_data[arc]:
                     xc = arc_data[arc]["xc"]
                     yc = arc_data[arc]["yc"]
@@ -83,9 +79,10 @@ def plot_map(limits, figname, canvas_size, exclude=0.0):
                     plt.gca().add_patch(c)
                     ax = -0.5*Rc*np.sin(PAc)
                     ay = 0.5*Rc*np.cos(PAc)
-                    plt.arrow(xx-xc, yy+yc, 4*ax, 4*ay, fc='none', ec=color, 
-                              width=0.0003, alpha=0.3, lw=0.3,
-                              head_width=2.0, head_length=4.0,
+                    plt.arrow(xx-xc, yy+yc, 4*ax*arrowscale, 4*ay*arrowscale,
+                              fc='none', ec=color, 
+                              width=0.001, alpha=0.8, lw=1.5,
+                              head_width=2.0*arrowscale, head_length=4.0*arrowscale,
                           )
 
 
@@ -140,8 +137,9 @@ if __name__ == "__main__":
 
     [x0], [y0] = coords.radec_str_to_decimal(["05:35:16.463"], ["-05:23:23.18"])
 
-    plot_map([-350, 600, -650, 200], "ll-positions.pdf", (20, 20), exclude=50.0)
-    plot_map([-50, 50, -35, 65], "ll-positions-zoom.pdf", (10, 10))
+    plot_map([-350, 600, -650, 200], "ll-positions.pdf", (10, 10),
+             exclude=50.0, arrowscale=2.0)
+    plot_map([-50, 50, -35, 65], "ll-positions-zoom.pdf", (10, 10), arrowscale=0.7)
 
 
 
