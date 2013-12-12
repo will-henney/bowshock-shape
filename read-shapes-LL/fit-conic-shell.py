72c1bcacc93a8b1260560c92a8dfc2d7d63bdc9c
Alejandro Tarango <j.tarango@crya.unam.mx>
2013-12-12 16:13:18 -0600

    Updating the version for fiiting theoretical arcs.
    Seems that this program works well, but it has some issues yet

--- read-shapes-LL/fit-conic-shell.py
+++ read-shapes-LL/fit-conic-shell.py
@@ -18,22 +18,33 @@ def update_arc_data(data,teo=False):
     Update the data dictionary for an arc with the hyperbola fit parameters
     """
     x, y = np.array(data["x"]), np.array(data["y"])
+    m = np.abs(np.degrees(np.arctan2(y, x))) - 105.0 <= 0.
     # Force the hyperbola axis to be along the circle fit axis
     # Initial guess is that center is also the same.
-    Rh, thh, PAh, xh, yh = conic_utils.fit_conic(
-        x, y, Rh=data["Rc"], thh=45.0, PAh=data["PAc"], xxh=data["xc"], yyh=data["yc"])
+    if teo:
+        Rh, thh, PAh, xh, yh = conic_utils.fit_conic(
+            x[m], y[m], Rh=data["Rc"], thh=-45.0, PAh=data["PAc"], xxh=data["xc"], yyh=data["yc"])
+    else:
+        Rh, thh, PAh, xh, yh = conic_utils.fit_conic(
+            x, y, Rh=data["Rc"], thh=45.0, PAh=data["PAc"], xxh=data["xc"], yyh=data["yc"])
+
     data.update(Rh=Rh, thh=thh, PAh=PAh, xh=xh, yh=yh)
     if cmd_args.savefig:
         plt.plot(-x, y, ".")
         xx,yy = conic_utils.world_hyperbola(Rh,thh,PAh,xh,yh) 
-        print(arc, ":", xh, yh, Rh, thh)
         if teo:
-            plt.plot(-xh, yh, "+r", ms=5.0)
-            plt.plot(-xx,yy,"-b",alpha=0.9)
+            ax.grid()
+            ax.set_xlim(-1.0,2)
+            ax.set_ylim(-2,2)
+            t=np.linspace(0,2*np.pi)
+            ax.plot(-data["xc"], data["yc"], "+r", ms=5.0)
+            ax.plot(-data["xc"]+data["Rc"]*np.cos(t),data["yc"]+data["Rc"]*np.sin(t),"g-",alpha=0.9)
+            ax.plot(-xx,yy,"m-",alpha=1.0)
+            ax.plot(xh,yh,"k+",ms=5.0)
         else:
             plt.plot(-xh, yh, "+" + colors[arc], ms=5.0)
             plt.plot(-xx,yy,colors[arc],alpha=0.9)
-        
+            print(arc, ":", xh, yh, Rh, thh)
         # c = plt.Circle((-xc, yc), radius=Rc,
         #                fc='none', ec=colors[arc], alpha=0.4, lw=0.2)
         # plt.gca().add_patch(c)
@@ -65,9 +76,11 @@ infile = cmd_args.source + "-arcdata.json"
 
 db = json.load(open(infile))
 
-if beta in infile:
-    for i in [0.0,15.0,30.0,45.0,60.0]:
-        update_arc_data(db["outer"]["i="+str(i)],teo=True)
+if "beta" in infile:
+    f = plt.figure()
+    for i,inc in enumerate([0.0,15.0,30.0,45.0,60.0]):
+        ax = f.add_subplot(2,3,i+1)
+        update_arc_data(db["outer"]["i="+str(inc)],teo=True)
 else:
     for arc in "inner", "outer":
         update_arc_data(db[arc])
@@ -84,9 +97,14 @@ os.rename(tempname, outfile)
 
 if cmd_args.savefig:
     plotfile = infile.replace("-arcdata.json", "-conic-fits.pdf")
-    plt.plot(0.0, 0.0, 'o')
-    plt.axis("equal")
-    plt.savefig(plotfile)
+    if "beta" in infile:
+        f.tight_layout()
+        f.set_size_inches(2*5,3*5)
+        f.savefig(plotfile)
+    else:
+        plt.plot(0.0, 0.0, 'o')
+        plt.axis("equal")
+        plt.savefig(plotfile)
 
 
 
