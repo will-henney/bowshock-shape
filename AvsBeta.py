cc40dd1905dee664d6b87e4562f8a76870cd8f80
William Henney <will@henney.org>
2014-09-24 23:33:19 -0500

    Distros of beta and mapping beta to other vars

--- AvsBeta.py
+++ AvsBeta.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import json
 
 import numpy as np
@@ -15,36 +16,38 @@ for inn,col in zip(shelltype,shellcolor):
         r0 = np.array(param["R0'"])
         rc = np.array(param["Rc"])
         inc = np.array(param["inc"])
-        plt.plot(np.array(float(beta)), r0[inc==0.0]/rc[inc==0.0], col,label=label)
+        plt.plot(np.array(float(beta)), rc[inc==0.0]/r0[inc==0.0], col,label=label)
         label =None
 
 def y(x, b, d): 
     c = np.exp(-1.0/b)
     return y0*(np.exp(-((x**d)/b)) - c)/(1.0 - c)
-print "Proplyd parameters"
+print("Proplyd parameters")
 y0 = 0.66
 b = 50.0
 d = 0.49
 x = np.linspace(0.0, 0.1, 500)
-plt.plot(x, y(x, b, d),label = "proplyd fit")
-print "b = ", b
-print "d = ", d
-print "exp(-1/b) = ", np.exp(-1.0/b)
-print "y0^-1 = ", 1./y0
+plt.plot(x, 1.0/y(x, b, d),label = "proplyd fit")
+print ("b = ", b                      )
+print ("d = ", d                      )
+print ("exp(-1/b) = ", np.exp(-1.0/b) )
+print ("y0^-1 = ", 1./y0              )
 
-print "Isotropic parameters"
+print ("Isotropic parameters")
 y0 = 0.585
 b = 5
 d = 0.5
-plt.plot(x,y(x, b, d),"g-",label="isotropic fit")
+plt.plot(x, 1.0/y(x, b, d),"g-",label="isotropic fit")
+
+plt.plot(x, 1.5/(1.0 - np.sqrt(x)), "r-", label='analytic')
 plt.legend(loc="best")
 plt.grid()
 plt.xlabel("beta")
-plt.ylabel("1/A")
+plt.ylabel("A")
 # plt.xlim(0.0, 1.0)
 # plt.ylim(0.0, 1.0)
 plt.savefig("AVSb.pdf")
-print "b = ", b
-print "d = ", d
-print "exp(-1/b) = ", np.exp(-1.0/b)
-print "y0^-1 = ", 1./y0
+print ("b = ", b                      )
+print ("d = ", d                      )
+print ("exp(-1/b) = ", np.exp(-1.0/b) )
+print ("y0^-1 = ", 1./y0              )  
