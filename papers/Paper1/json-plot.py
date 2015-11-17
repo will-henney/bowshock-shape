e88ab1e2b172a6572a6f527cf0935138f09d1ca6
Alejandro Tarango <j.tarango@crya.unam.mx>
2015-11-17 10:18:37 -0600

    Python scripts for making new graphs.

--- papers/Paper1/json-plot.py
+++ papers/Paper1/json-plot.py
@@ -15,7 +15,7 @@ import sys
 parser = argparse.ArgumentParser(description="Plot output options")
 
 parser.add_argument("-x",type=str,default="i",choices=["i","I","R0","r0","beta","b"],help="x axis quantity")
-parser.add_argument("-y",type=str,default="Rc",choices=["Rc","rc","R0","r0"],help="y axis quantity")
+parser.add_argument("-y",type=str,default="Rc",choices=["Rc","rc","R0","r0","r90","R90"],help="y axis quantity")
 #the choices are made in a such way that does not distinguish between uppercases and lowercases, also beta and b
 #are equivalent choices
 
@@ -26,9 +26,10 @@ r0_choice_x = (args.x == "r0") or (args.x == "R0")
 r0_choice_y = (args.y == "r0") or (args.y == "R0")
 beta_choice = (args.x == "beta") or (args.x == "b")
 rc_choice = (args.y == "Rc") or (args.y == "rc")
-print("You chose to plot {} vs {}".format(args.x,args.y))
+r90_choice = (args.y == "R90") or (args.y == "r90")
+print("You chose to plot {} vs {}".format(args.y,args.x))
 
-print(r0_choice_x,"\n",r0_choice_y,"\n",inc_choice,"\n",beta_choice,"\n",rc_choice)
+#print(r0_choice_x,"\n",r0_choice_y,"\n",inc_choice,"\n",beta_choice,"\n",rc_choice)
 #chosing R0 as x axis and y axis should lead to the identity function
 if r0_choice_x and r0_choice_y:
     print("These choices don't lead to useful information")
@@ -66,47 +67,82 @@ matplotlib.rcParams.update(params)
 if r0_choice_x:
     shelldata = json.load(open("RcVsR0.json"))
     shelldata1 = json.load(open("Rc-R0VsBeta.json"))
-
+    if rc_choice:
+   #     Y = Rc
+        ylab =r"$R'_c/R'_0$"
+        out = "Rc-R0-b.pdf"
+        out1 = "Rc-R0-i.pdf"
+        
+        
+    elif r90_choice:
+    #    Y = R90
+        ylab = r"$R'_{90}/R'_0$"
+        out = "R90-R0-b.pdf"
+        out1 = "R90-R0-i.pdf"
     for b, modeldata in shelldata.items():
         inclinations = shelldata[b]["inc"]
         R0 = np.array(shelldata[b]["R0'"])
-        Rc = np.array(shelldata[b]["Rc'"])
-        plt.plot(R0,Rc/R0,label=r"$\beta={}$".format(b))
+        Rc = np.array(shelldata[b]["Rc'_f"])
+        R90 = np.array(shelldata[b]["R90'"])
+        if rc_choice:
+            Y=Rc/R0
+            #dY = np.array(shelldata[b]["dRc'"])
+            plt.plot(R0,Y,label=r"$\beta={}$".format(b))
+        else:
+            Y=R90/R0
+            plt.plot(R0,Y,label=r"$\beta={}$".format(b))
     plt.legend(fontsize="small")
     plt.xlabel(r"$R'_0/D'$")
-    plt.ylabel(r"$R'_c/R'_0$")
-    plt.savefig("Rc-R0-b.pdf")
+    plt.ylabel(ylab)
+    plt.xlim(-1e-4,0.5+1e-4)
+    plt.ylim(-1e-4,8+1e-4)
+    plt.savefig(out)
     plt.clf()
 
     for i, modeldata in shelldata1.items():
         beta = np.array(shelldata1[i]["beta"])
         R0 = np.array(shelldata1[i]["R0'"])
-        Rc = np.array(shelldata1[i]["Rc'"])
-        plt.plot(R0,Rc/R0,label=r"$i={}$".format(i))
+        Rc = np.array(shelldata1[i]["Rc'_f"])
+        R90 = np.array(shelldata1[i]["R90'"])
+        if rc_choice:
+            Y=Rc/R0
+            #dY = np.array(shelldata1[i]["dRc'"])
+            plt.plot(R0,Y,label=r"$i={}$".format(i))
+        else:
+            Y=R90/R0
+            plt.plot(R0,Y,label=r"$i={}$".format(i))
     plt.legend(fontsize="small")
     plt.xlabel(r"$R'_0/D'$")
-    plt.ylabel(r"$R'_c/R'_0$")
-    plt.savefig("Rc-R0-i.pdf")
+    plt.ylabel(ylab)
+    plt.xlim(-1e-4,0.5+1e-4)
+    plt.ylim(-1e-4,8+1e-4)
+    plt.savefig(out1)
 
 elif inc_choice:
     shelldata = json.load(open("RcVsR0.json"))
     for b, modeldata in shelldata.items():
         inclinations = np.array(shelldata[b]["inc"])
         R0 = np.array(shelldata[b]["R0'"])
-        Rc = np.array(shelldata[b]["Rc'"])
+        Rc = np.array(shelldata[b]["Rc'_f"])
+        R90 = np.array(shelldata[b]["R90'"])
         if r0_choice_y:
             Y=R0
+            ylabel=r"$R'_0/D$"
+            out = "R0-i.pdf"
+            dy = None
         elif rc_choice:
-            Y=Rc/R0
+            Y=Rc
+            ylabel=r"$R'_c/D'$"
+            out="Rc-i.pdf"
+           # dy = np.array(shelldata[b]["dRc'"])
+        elif r90_choice:
+            Y = R90
+            ylabel=r"$R'_{90}/D'$"
+            out="R90-i.pdf"
+            #dy = None
         plt.plot(inclinations,Y,label=r"$\beta={}$".format(b))
     plt.legend(fontsize="small")
     plt.xlabel("inclination (deg)")
-    if r0_choice_y:
-        ylabel=r"$R'_0/D$"
-        out = "R0-i.pdf"
-    elif rc_choice:
-        ylabel=r"$R'_c/D'$"
-        out="Rc-i.pdf"
     plt.ylabel(ylabel)
     plt.savefig(out)
     
@@ -115,20 +151,26 @@ elif  beta_choice:
     for i, modeldata in shelldata.items():
         beta = np.array(shelldata[i]["beta"])
         R0 = np.array(shelldata[i]["R0'"])
-        Rc = np.array(shelldata[i]["Rc'"])
+        Rc = np.array(shelldata[i]["Rc'_f"])
+        R90 = np.array(shelldata[i]["R90'"])
         if r0_choice_y:
             Y=R0
-        else:
-            Y=Rc/R0
+            ylabel=r"$R'_0/D'$"
+            out = "R0-b.pdf"
+            #dy=None
+        elif rc_choice:
+            Y=Rc
+            ylabel=r"$R'_c/D'$"
+            out = "Rc-b.pdf"
+            #dy = np.array(shelldata[i]["dRc'"])
+        elif r90_choice:
+            Y = R90
+            ylabel=r"$R'_{90}/D'$"
+            out = "R90-b.pdf"
+            #dy=None
         plt.plot(beta,Y,label=r"$i={}$".format(i))
     plt.legend(fontsize="small")
     plt.xlabel(r"$\beta$")
-    if r0_choice_y:
-        ylabel=r"$R'_0/D$"
-        out = "R0-b.pdf"
-    elif rc_choice:
-        ylabel=r"$R'_c/D'$"
-        out="Rc-b.pdf"
     plt.ylabel(ylabel)
     plt.savefig(out)
 
--- papers/Paper1/toolbox.py
+++ papers/Paper1/toolbox.py
@@ -5,7 +5,8 @@ characteristic radii, etc.
 """
 
 import numpy as np
-
+from scipy.interpolate import interp1d
+from scipy.optimize import leastsq
 def omega(R,t):
     """
     Calculates the deivate of ln(R(theta))
@@ -43,12 +44,69 @@ def R_prime(xp,yp):
     """
     return np.sqrt(xp**2+yp**2)
 
+def R0p(R,t):
+    """
+    Returns R'_0 and a uncertainty due to the first element of theta' is not
+    (almost sure) exactly 0 
+    """
+
+
+    r0p = R[0]
+    w = omega(R,t)
+    dr = np.abs(r0p*w[0]*t[0])
+    return r0p,dr
+
 def Rc(R,t):
     """
-    Computes the radius of curvature at the shell's <<nose>>
+    Computes the radius of curvature at the shell's <<nose>> and the error margin.
     This is a numeric approach, so, for i=0, the result will differ from the analytic 
     result, but is accurate enough. Note: Enter projected R & theta
     """
     w = omega(R,t)
     dw = np.gradient(w)/np.gradient(t) # Derivative of omega
-    return R[0]*(w[0]**2+1)**1.5/np.abs(w[0]**2-dw[0]+1)
+    rc = R[0]/np.abs(-dw[0]+1)
+    f = (w**2+1)**1.5/np.abs(w**2-dw+1)
+    df = np.gradient(f)/np.gradient(t)
+    drc = np.abs(R[0]*(df[0])*t[0])
+    return rc,drc
+
+def Rc_fit(R,t):
+    """
+    Compute Rc via least squares fit
+    (the old fashion way)
+    """
+    def complete_curve(R,t):
+        """
+        return the complete shape of the bowshock in cartesian coordinates
+        """
+        theta_com = np.concatenate((sorted(-t),t))
+        R_com = np.concatenate((sorted(R,reverse=True),R))
+        x_com,y_com = R_com*np.cos(theta_com),R_com*np.sin(theta_com)
+        return x_com,y_com
+    
+    def R_calc(xc,yc):
+        """
+        calculate the distance between the (x,y) curve and the center (xc,yc)
+        """
+        return np.sqrt((xfit-xc)**2+(yfit-yc)**2)
+    def f_fit(c):
+        Ri = R_calc(*c)
+        return Ri-Ri.mean()
+    x,y = complete_curve(R,t)
+    m = np.abs(np.degrees(np.arctan2(y,x)))-45. <=0
+    xfit,yfit = x[m],y[m]
+    c0 = 0,0
+    c1,ier = leastsq(f_fit,c0)
+    xf,yf = c1
+    ri = R_calc(xf,yf)
+    return ri.mean()
+
+def R90(R,t):
+    """
+    Computes the radius of the shell perpendicular to the symmetry axis. And the 
+    corresponding errorbar. Insert the projected R and theta.
+    """
+
+    r_int = interp1d(t,R,bounds_error= False)
+    R90 = r_int(0.5*np.pi)
+    return R90
