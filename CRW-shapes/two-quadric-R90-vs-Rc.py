c0e6c21da6627e4beb9165605bb2328316de7133
William Henney <will@henney.org>
2017-02-21 22:05:01 -0600

    Switch to R90-Rc graph instead of Tc-Rc
    Done this for the bi-quadric graph. Now for the single ones

--- CRW-shapes/two-quadric-R90-vs-Rc.py
+++ CRW-shapes/two-quadric-R90-vs-Rc.py
@@ -9,29 +9,26 @@ plotfile = sys.argv[0].replace('.py', '.pdf')
 
 sns.set_style('white')
 fig, ax = plt.subplots(figsize=(5, 5))
-fig2, ax2 = plt.subplots(figsize=(5, 5))
+
+
+
+Rc_grid = np.linspace(0.0, 10.0, 2000)
+R90_T0_grid = np.sqrt(2*Rc_grid)
+R90_T1_grid = np.sqrt(2*Rc_grid - 1.0)
+R90_T1_grid[~np.isfinite(R90_T1_grid)] = 0.0 
+ax.fill_between(Rc_grid, R90_T1_grid, R90_T0_grid, alpha=0.2)
+ax.plot([0.0, 10.0], [0.0, 10.0], ':', c='k')
 
 inc = np.linspace(0.0, 0.5*np.pi, 500, endpoint=False)
 inc_deg = np.degrees(inc)
 
 
 XI_LIST = [None, 1.0, 0.8, 0.4]
-BETA_LIST = [0.1, 0.03, 0.01, 0.001, 0.0001, 0.00001]
+BETA_LIST = [0.2, 0.1, 0.05, 0.02, 0.01, 0.001, 0.0001, 0.00001]
 nxi, nbeta = len(XI_LIST), len(BETA_LIST)
 
-dashes_solid = []
-dashes_dashed = [3, 2]
-dashes_dotted = [1, 2]
-dashes_dot_dashed = [1, 2, 4, 2]
-dashes_triple_dot_dashed = [1, 2, 1, 2, 1, 2, 4, 2]
-dashes_styles = [dashes_solid, dashes_dashed, dashes_dotted,
-                 dashes_dot_dashed, dashes_triple_dot_dashed,
-                 dashes_solid, dashes_solid]
-
 cols = sns.color_palette('magma', n_colors=nxi)
-
-
-for beta, dashes in list(zip(BETA_LIST, dashes_styles))[::-1]:
+for beta in BETA_LIST[::-1]:
     for xi, col in list(zip(XI_LIST, cols))[::-1]:
 
         # Fit to head and analytic fit to fit to tail
@@ -88,9 +85,10 @@ for beta, dashes in list(zip(BETA_LIST, dashes_styles))[::-1]:
         #     label = fr'$\xi = {xi:.1f}$'
         # else:
         #     label = None
-        xilabel = 'CRW' if xi is None else fr'$\xi = {xi:.1f}$'
-        betalabel = fr'$\beta = {beta:.5f}$'
-        label = f'{xilabel} {betalabel}'
+        if beta == BETA_LIST[0]:
+            label = 'CRW' if xi is None else fr'$\xi = {xi:.1f}$'
+        else:
+            label = None
 
         # Find minimum difference between head and tail values of R90
         dR = np.abs(R90_h_prime - R90_t_prime)
@@ -104,48 +102,27 @@ for beta, dashes in list(zip(BETA_LIST, dashes_styles))[::-1]:
         # Ensure that LOS is not inside the tail cone
         mhi = mhi & (inc < 0.5*np.pi - ht.theta_t)
         # Plot the head discriminant for low inclinations
-        ax.plot(tilde_Rc_h_prime[mlo], T_h_prime[mlo],
-                c=col, label=label, lw=2, alpha=0.4)
+        ax.plot(tilde_Rc_h_prime[mlo], R90_h_prime[mlo]/R0_h_prime[mlo],
+                c=col, label=None, lw=2, alpha=0.4)
         # Put a dot at the i=0 case
-        ax.plot([tilde_Rc_h], [T_h], 'o', c=col)
+        ax.plot([tilde_Rc_h], [R90_h/R0_h], 'o', c=col, alpha=0.6)
         # Plot the combined discriminant for high inclinations
-        ax.plot(tilde_Rc_h_prime[mhi], T_combine_prime[mhi],
-                c=col, label=None, lw=0.8, alpha=1.0)
+        ax.plot(tilde_Rc_h_prime[mhi], R90_t_prime[mhi]/R0_h_prime[mhi],
+                c=col, label=label, lw=0.8, alpha=1.0)
         #ax.plot([tilde_Rc_h], [T_combine], '.', c=col)
 
-        ax2.plot(inc_deg, R90_h_prime/R0_h_prime,
-                 c=col, dashes=dashes, label=label, lw=2, alpha=0.4)
-        ax2.plot(inc_deg, R90_t_prime/R0_h_prime,
-                 c=col, dashes=dashes, label=None, lw=0.8, alpha=1.0)
-        ax2.plot([inc_deg[i0]], [R90_t_prime[i0]/R0_h_prime[i0]], 'o', c=col)
-
-
-#ax.legend(ncol=1, fontsize='xx-small', frameon=True)
+ax.legend(ncol=1, fontsize='xx-small', frameon=True)
 ax.set(
     yscale='linear',
     xscale='linear',
-    xlim=[0.0, 4.1],
-    ylim=[-8.0, 1.1],
+    xlim=[0.0, 5.1],
+    ylim=[0.0, 5.1],
 #    ylim=[-3.0, 1.1],
     xlabel=r"Projected dimensionless radius of curvature: $\widetilde{R}_{c}{}'$",
-    ylabel=r"Projected conic discriminant: $T_c{}'$",
+    ylabel=r"Projected dimensionless perpendicular radius: $\widetilde{R}_{90}{}'$",
 )        
 
 
 fig.tight_layout()
 fig.savefig(plotfile)
 print(plotfile, end='')
-
-ax2.legend(ncol=3, fontsize='xx-small', frameon=True)
-ax2.set(
-    yscale='linear',
-    xscale='linear',
-    xlim=[0.0, 90.0],
-    ylim=[0.0, 6.0],
-#    ylim=[-3.0, 1.1],
-    xlabel=r"Inclination angle, degrees",
-    ylabel=r"Projected perpendicular radius: $R_{90}'$",
-)        
-
-fig2.tight_layout()
-fig2.savefig(plotfile.replace('Tc-vs-Rc', 'R90-vs-i'))
