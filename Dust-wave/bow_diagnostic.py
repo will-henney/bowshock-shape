9ebf5ff4d336afd931fe3efca0089f8d6a0268ad
William Henney <will@henney.org>
2017-10-10 17:26:29 -0500

    Make tables of R_c-R_90 for Wilkinoid and Cantoids

--- Dust-wave/bow_diagnostic.py
+++ Dust-wave/bow_diagnostic.py
@@ -1,6 +1,6 @@
 import numpy as np
 from astropy.table import Table
-from bow_projection import characteristic_radii_projected
+import bow_projection as bp
 
 def parameter_table(inclinations, shape, *shape_args):
     """Diagnostic parameters for `inclinations` from `shape`
@@ -14,25 +14,31 @@ functions suitable for passing as `shape` can be found in the
 Returns an `astropy.table.Table` of characteristic angles and radii.
 
     """
-    rows = [characteristic_radii_projected(inc, shape, *shape_args)
+    rows = [bp.characteristic_radii_projected(inc, shape, *shape_args)
             for inc in inclinations]
     tab = Table(rows=rows)
     tab['inc'] = inclinations
+    # Number formatting in output
+    for k in tab.colnames:
+        if 'theta' in k or 'inc' in k:
+            # Angle columns: convert to degrees, give to 1 decimal place
+            tab[k] = np.degrees(tab[k]).round(decimals=1)
+        else:
+            # Radii columns: give to 4 decimal places
+            tab[k] = tab[k].round(decimals=4)
     return tab
 
 
 if __name__ == '__main__':
-    from bow_projection import (wilkinoid_R_theta, cantoid_R_theta,
-                                Spline_R_theta_from_function, theta_infinity)
-
-    th_inf = theta_infinity(cantoid_R_theta, 0.001)
+    bp.DEBUG = True
+    th_inf = bp.theta_infinity(bp.cantoid_R_theta, 0.001)
     inclinations = np.linspace(0.0, th_inf - np.pi/2, 30)
-    tab = parameter_table(inclinations, cantoid_R_theta, 0.001)
+    tab = parameter_table(inclinations, bp.cantoid_R_theta, 0.001)
     Rc, R90 = tab['tilde R_c prime'], tab['tilde R_90 prime']
     tab_s = parameter_table(inclinations,
-                            Spline_R_theta_from_function(
+                            bp.Spline_R_theta_from_function(
                                 ngrid=1000,
-                                shape_func=cantoid_R_theta,
+                                shape_func=bp.cantoid_R_theta,
                                 shape_func_pars=(0.001,)))
     Rc_s, R90_s = tab_s['tilde R_c prime'], tab_s['tilde R_90 prime']
 
