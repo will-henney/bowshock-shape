448691da088fe9e692398779568ae42227fb0773
William Henney <will@henney.org>
2017-12-11 10:54:52 -0600

    Finish graph of wind BS - rad BW - rad BS

--- Dust-wave/zones-v-n-plane.py
+++ Dust-wave/zones-v-n-plane.py
@@ -6,6 +6,8 @@ from vec_root import chandrupatla
 
 figname = sys.argv[0].replace('.py', '.pdf')
 
+sns.set_style('ticks')
+sns.set_color_codes('dark')
 fig, axes = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(8, 4))
 stardata = [
     [10.0, 0.63, 0.0026, axes[0]],
@@ -16,7 +18,7 @@ stardata = [
 # Velocities in units of km/s (10 km/s -> 100 km/s)
 vgrid = np.linspace(10.0, 100.0, 200)
 # Densities in units of 1 pcc  (0.01 -> 1e5)
-logngrid = np.linspace(-3.0, 6.0, 200)
+logngrid = np.linspace(-3.3, 6.3, 200)
 # 2d versions of velocity and density grids
 vv, nn = np.meshgrid(vgrid, 10**logngrid)
 
@@ -32,20 +34,49 @@ def xfunc(x, ts, eta):
     """Function to be zeroed to find x"""
     return x**2 - (1.0 - np.exp(-2*ts*x)) - eta
 
-R0s = (0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10.0, 30.0)
+R0s = (0.0003, 0.001, 0.003, 0.01, 0.03,
+       0.1, 0.3, 1.0, 3.0, 10.0, 30.0)
+lws = np.linspace(0.3, 2.5, len(R0s))
+cformats = { 0.0001: "0.0001 pc", 0.001: "0.001 pc", 0.01: "0.01 pc", 0.1:
+             "0.1 pc", 1.0: "1 pc", 10.0: "10 pc", }
+clevs_to_label = list(cformats.keys())
+
+box_params = dict(fc='w', ec='0.8', lw=0.4, pad=2)
+RBW_label = r"Radiation bow wave, $\eta < \tau < 1$"
+RBS_label = r"Radiation bow shock, $\tau > 1$"
+WBS_label = r"Wind bow shock, $\tau < \eta$"
+
 for M, L4, eta, ax in stardata:
+    Mlabel = "\n".join([rf"$M = {M:.0f}\, M_\odot$",
+                        rf"$L = {1e4*L4:.1e}\, L_\odot$".replace("e+0", r"\times 10^"),
+                        rf"$\eta = {eta}$"])
     Rs = rstar(vv/10, nn, L4)
     ts = taustar(vv/10, nn, L4)
     a, b = 0.0, 2*np.sqrt(1.0 + eta)
     x = chandrupatla(xfunc, a, b, args=(ts, eta))
     R0 = x*Rs
     tau = 2*x*ts
-    ax.contour(vv, nn, tau, (eta/3, eta, 3*eta), colors='r')
-    ax.contour(vv, nn, tau, (1.0, 3.0), colors='m')
-    ax.contour(vv, nn, R0, R0s, colors='b')
+    ax.contourf(vv, nn, tau, (eta, 1.0), colors='k', alpha=0.2)
+    # ax.contour(vv, nn, tau, (eta/3, eta, 3*eta), colors='r')
+    # ax.contour(vv, nn, tau, (1.0, 3.0), colors='m')
+    cs = ax.contour(vv, nn, R0, R0s, linewidths=lws, colors='k')
+    clevs = [level for level in clevs_to_label if level in cs.levels]
+    ax.clabel(cs, clevs,
+              fontsize='small', fmt=cformats,
+              inline=True, inline_spacing=2, use_clabeltext=True)
+    ax.text(30.0, 1e-2, Mlabel, fontsize='small', bbox=box_params)
+    ax.text(18.0, 4000.0, RBW_label, rotation=15, fontsize='small', bbox={**box_params, **dict(fc='0.8', ec='0.6')})
+    ax.text(18.0, 2e6, RBS_label, rotation=15, fontsize='small', bbox=box_params)
+    ax.text(18.0, 30.0, WBS_label, rotation=15, fontsize='small', bbox=box_params)
+
+
+
     ax.set(yscale='log')
 
-axes[0].set(xlabel=r"$v$, km/s", ylabel=r"$n$, pcc")
+axes[0].set(xlabel=r"$v$, km s$^{-1}$", ylabel=r"$n$, cm$^{-3}$")
+sns.despine()
+for ax in axes:
+    ax.label_outer()
 fig.tight_layout()
 fig.savefig(figname)
 
