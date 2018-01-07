803a508a2e45ead67906367fd9517741d08add12
William Henney <will@henney.org>
2018-01-06 23:58:48 -0600

    Write WCS info to cubes

--- Dust-wave/stitch-bfield-cube.py
+++ Dust-wave/stitch-bfield-cube.py
@@ -31,20 +31,24 @@ yy = y[None, :, None]
 zz = z[:, None, None]
 rsq = xx**2 + yy**2 + zz**2
 
+w = WCS(naxis=3)
+w.wcs.crpix = [1, 1, 1]
+w.wcs.crval = [x[0], y[0], z[0]]
+w.wcs.cdelt = [x[1] - x[0], y[1] - y[0], z[1] - z[0]]
 
 # Write out cube of density
 fits.PrimaryHDU(
-    data=cube
+    data=cube, header=w.to_header(),
 ).writeto(f"dust-bfield-b{ANGLE}-cube.fits", overwrite=True)
 # And cube of density * flux (Assuming ~ 1/R^2)
 fits.PrimaryHDU(
-    data=cube/rsq
+    data=cube/rsq, header=w.to_header(),
 ).writeto(f"dust-bfield-b{ANGLE}-cube-F.fits", overwrite=True)
 # And cube of density * T (Assuming T^4 ~ Flux)
 fits.PrimaryHDU(
-    data=cube/rsq**0.25
+    data=cube/rsq**0.25, header=w.to_header(),
 ).writeto(f"dust-bfield-b{ANGLE}-cube-T.fits", overwrite=True)
 # And cube of density * T^2 (Assuming T^4 ~ Flux)
 fits.PrimaryHDU(
-    data=cube/rsq**0.5
+    data=cube/rsq**0.5, header=w.to_header(),
 ).writeto(f"dust-bfield-b{ANGLE}-cube-T2.fits", overwrite=True)
