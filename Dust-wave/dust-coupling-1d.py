e9857104d819f51e3cf9172c9161d4099c3ad428
William Henney <will@henney.org>
2018-01-16 18:10:32 -0600

    Fix error in drag equation - should be v-squared

--- Dust-wave/dust-coupling-1d.py
+++ Dust-wave/dust-coupling-1d.py
@@ -4,11 +4,13 @@ from scipy.integrate import odeint
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-def dydt(y, t, alpha):
+def dydt(y, t, alpha, sound=0.5):
     """Derivatives for ODE: x'' = 0.5 (x^{-2} - alpha^2 (x' + 1))"""
     x, u = y
     dxdt = u
-    dudt = 0.5*(x**(-2) - alpha**2 * (u + 1.0))
+    dudt = 0.5*(x**(-2)
+                - alpha**2 * (u + 1.0)*np.sqrt((1.65*sound)**2
+                                               + (u + 1.0)**2))
     return [dxdt, dudt]
 
 figfile = sys.argv[0].replace('.py', '.pdf')
@@ -17,7 +19,7 @@ figfile = sys.argv[0].replace('.py', '.pdf')
 y0 = [10.0, -1.0]
 
 # Coupling parameter
-alpha = 1.0/2.0
+alpha = 0.4
 
 # Time grid
 t = np.linspace(0.0, 60.0, 501)
@@ -41,11 +43,13 @@ ax.axhspan(0.0, 1.0, color='k', alpha=0.1)
 ax.legend(title=r"$\alpha_\mathrm{drag} = 0.5$")
 ax.set(xlabel=r'Time / $(R_{0} / v_{\infty})$', ylim=[-0.3, 4.3])
 t2 = np.linspace(0.0, 20.0, 201)
+soln1 = odeint(dydt, y0, t2, args=(1.0,))
 soln2 = odeint(dydt, y0, t2, args=(2.0,))
 soln0 = odeint(dydt, y0, t2, args=(0.0,))
 
 axp.plot(soln0[:, 0], soln0[:, 1], label=r"$\alpha_\mathrm{drag} = 0$")
 axp.plot(soln[:, 0], soln[:, 1], label=r"$\alpha_\mathrm{drag} = 0.5$")
+axp.plot(soln1[:, 0], soln1[:, 1], label=r"$\alpha_\mathrm{drag} = 1$")
 axp.plot(soln2[:, 0], soln2[:, 1], label=r"$\alpha_\mathrm{drag} = 2$")
 axp.axhline(0, xmax=0.55, color='k', lw=0.5)
 axp.legend(title='Phase space\n  trajectories')
