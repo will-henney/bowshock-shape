aa0e34bf3f32d9c0121a65d5b552c24f620b1648
William Henney <will@henney.org>
2018-02-21 13:42:42 -0600

    Finish Meyer tracing fig and write about it (unfinished)

---
+++ papers/Papers1234/auto/app-mhd-sims.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook
+ "app-mhd-sims"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:tracing-bow-shapes"
+    "fig:meyer-trace"))
+ :latex)
+
--- papers/Papers1234/auto/sec-conclusions.el
+++ papers/Papers1234/auto/sec-conclusions.el
@@ -1,15 +1,16 @@
 (TeX-add-style-hook
  "sec-conclusions"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
     "sec:more-realistic-bow"
+    "fig:meyer-trace"
     "fig:sim-depart"
     "fig:sim-xyp"
     "fig:sim-Pi-Lambda"
