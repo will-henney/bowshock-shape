47042852163bc82993ffde824841df94cb8c5661
William Henney <will@henney.org>
2018-02-27 13:56:30 -0600

    Write more about the 000-400 fits in the text

--- papers/Papers1234/auto/sec-conclusions.el
+++ papers/Papers1234/auto/sec-conclusions.el
@@ -1,12 +1,12 @@
 (TeX-add-style-hook
  "sec-conclusions"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
     "sec:more-realistic-bow"
@@ -19,6 +19,7 @@
     "fig:sim-histograms"
     "fig:000-400-fit"
     "fig:000-400-planitude-alatude"
+    "fig:000-400-Delta-theta"
     "sec:obs"
     "sec:conc"))
  :latex)
