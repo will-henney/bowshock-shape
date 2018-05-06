5ecd75d1ae4bd06da4f8eaac3694e11e87d47a03
William Henney <will@henney.org>
2018-05-05 23:54:33 -0500

    Finally finish decoupling parameter space graph

--- Dust-wave/coulomb-decouple-v-n-plane.py
+++ Dust-wave/coulomb-decouple-v-n-plane.py
@@ -7,17 +7,18 @@ from vec_root import chandrupatla
 figname = sys.argv[0].replace('.py', '.pdf')
 
 sns.set_style('ticks')
-sns.set_color_codes('dark')
+sns.set_color_codes('bright')
 fig, axes = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(8, 4))
 stardata = [
     [10.0, 0.63, 0.0066, 1e-4, axes[0]],
     [20.0, 5.453, 0.1199, 0.1, axes[1]],
     [40.0, 22.19, 0.4468, 1.0, axes[2]],
 ]
+MICRON = 1e-4                   # cm
 
 # Velocities in units of km/s (10 km/s -> 100 km/s)
 vgrid = np.linspace(10.0, 100.0, 800)
-vgrid = np.logspace(0.8, 2.8, 800)
+vgrid = np.logspace(1.1, 3.1, 800)
 # Densities in units of 1 pcc  (0.01 -> 1e5)
 logngrid = np.linspace(-3.3, 6.3, 800)
 # 2d versions of velocity and density grids
@@ -84,10 +85,23 @@ Habing = 1.6e-3
 Lsun = 3.82e33
 pc = 3.085677582e18
 
-G_n_jump = {
-    10.0: 3e4,
-    20.0: 1e5,
-    40.0: 1e5
+# Values for graphite
+Xi_dag = {
+    "gra": {
+        10.0: 1000.0,
+        20.0: 3000.0,
+        40.0: 3000.0
+    },
+    "sil": {
+        10.0: 350.0,
+        20.0: 2500.0,
+        40.0: 2500.0
+    },
+}
+Qp = 2.0
+rho_d = {
+    "gra": 2.2,
+    "sil": 3.5,
 }
 
 T0 = 8000
@@ -129,20 +143,6 @@ for M, L4, eta, S49, ax in stardata:
     Uout[~np.isfinite(Uout)] = 0.0
     Ushout = Uout/(vv/10)**2
 
-    # FUV excitation parameter - fiducial (at R0 and no extinction)
-    # G_n = 1e4*L4*Lsun*frac_fuv[M]*np.exp(-tau_dust) / (4*np.pi * (R0*pc)**2 * Habing * nn)
-    G_n = 1e4*L4*Lsun*frac_fuv[M] / (4*np.pi * (Rstarstar*pc)**2 * Habing * nn)
-    G_n_bs = 1e4*L4*Lsun*frac_fuv[M] / (4*np.pi * (R0*pc)**2 * Habing * nn)
-    # print(G_n.min(), G_n.max(), np.median(G_n))
-
-    # y^2 / (1 - y) = CU
-    # y = 0.1 => y^2 / (1 - y) = 0.0111111111111
-    # y = 0.5 => y^2 / (1 - y) = 0.5
-    # y = 0.9 => y^2 / (1 - y) = 8.1
-    # y = 0.99 => y^2 / (1 - y) = 98.1
-
-    # cu = 3.5e5*Uout
-
     c_sig_over_alpha = 2.99792458e10*3e-18 / alphaB
     c_sig_over_alpha *= (1 - absfrac)**(1./3.)
     y_IF = 0.1
@@ -155,56 +155,27 @@ for M, L4, eta, S49, ax in stardata:
     tau_dust_IF = np.nanmean(tau_dust[m])
     cs = ax.contourf(vv, nn, tau_dust, (tau_dust[m].min(), tau_dust[m].max()), linewidths=2, colors='r', alpha=0.3)
     ax.contour(vv, nn, absfrac, 1.0, linewidths=0.7, colors='r')
+    ax.contourf(vv, nn, tau, (eta, 1.0), colors='k', alpha=0.2)
 
-    # arrows = r"$\uparrow\!\!\!\!\uparrow$"
-    # trap_text = rf"{arrows} Trapped i-front {arrows}"
-    # ax.text(60, d["trapped y"][M],
-    #         trap_text,
-    #         ha='center', va='center',
-    #         fontsize='xx-small', color='r', alpha=0.5, rotation=10,
-    #         bbox=dict(fc=d["trapped bg"][M], ec='none', pad=0.1)
-    # )
+    for grain, a, color in [
+            ["gra", 0.02, "y"],
+            ["gra", 0.2, "m"],
+            ["sil", 0.02, "c"],
+            ["sil", 0.2, "b"],
+    ]:
+        kappa_d = 3.0*Qp/(4.0*a*MICRON*rho_d[grain])
+        mdw = (
+            (ts > 0.5*np.sqrt(eta)*kappa/kappa_d)
+            & (ts < 0.5*(vv/10.0)/np.sqrt(Xi_dag[grain][M]))
+            & (vv/10 > np.sqrt(eta*Xi_dag[grain][M]))
+        ).astype(float)
+        cs = ax.contourf(vv, nn, mdw, [0.5, 1.5], colors=color, alpha=0.3)
 
-    wdrift = drift_velocity(G_n, G_n_jump[M])
-    alpha_drag = vv / wdrift
-
-    wdrift_bs = drift_velocity(G_n_bs, G_n_jump[M])
-    alpha_bs = vv / wdrift_bs
-
-    m_bs = alpha_bs < 1.0
-    #alpha_drag[~m] = np.nan
-
-    cs = ax.contourf(vv, nn, alpha_drag, (0.0, 20.0),
-                    linewidths=1, colors='y', alpha=0.05)
-    cs = ax.contourf(vv, nn, alpha_drag, (0.0, 1.0),
-                    linewidths=1, colors='y', alpha=0.3)
-    cs = ax.contourf(vv, nn, alpha_bs, (0.0, 2.0),
-                    linewidths=1, colors='m', alpha=0.05)
-    cs = ax.contourf(vv, nn, alpha_bs, (0.0, 1.0),
-                    linewidths=1, colors='m', alpha=0.2)
-    cs = ax.contourf(vv, nn, alpha_bs, (0.0, 0.5),
-                    linewidths=1, colors='m', alpha=0.3)
-
-    Rdw = Rstarstar / (1.0 + alpha_drag)
-
-    # Remove cases where dust wave is inside bow shock
-    m_dw = (Rdw >= R0) & m_bs
-    Rdw[~m_dw] = np.nan
-    R0[m_dw] = np.nan
-
-
-
-    ax.contourf(vv, nn, tau, (eta, 1.0), colors='k', alpha=0.15)
     cs = ax.contour(vv, nn, R0, R0s, linewidths=lws, colors='k')
     clevs = [level for level in clevs_to_label if level in cs.levels]
     ax.clabel(cs, clevs,
               fontsize='x-small', fmt=cformats,
               inline=True, inline_spacing=2, use_clabeltext=True)
-    cs = ax.contour(vv, nn, Rdw, R0s, linewidths=lws, colors='b')
-    # clevs = [level for level in clevs_to_label if level in cs.levels]
-    # ax.clabel(cs, clevs,
-    #           fontsize='x-small', fmt=cformats,
-    #           inline=True, inline_spacing=2, use_clabeltext=True)
     ax.text(100.0, 1e5, Mlabel, zorder=100, fontsize='x-small', bbox=box_params)
 
     #
