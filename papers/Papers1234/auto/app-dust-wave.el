de9e18b276f1b44c5f090341ed468028ba784048
William Henney <will@henney.org>
2018-06-21 13:29:11 -0500

    New figure of the perpendicular field case for the appendix

--- papers/Papers1234/auto/app-dust-wave.el
+++ papers/Papers1234/auto/app-dust-wave.el
@@ -1,12 +1,12 @@
 (TeX-add-style-hook
  "app-dust-wave"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-add-symbols
     "Larmor")
@@ -28,6 +28,7 @@
     "eq:dust-wdrift"
     "eq:dust-alpha"
     "sec:bow-wave-with"
+    "fig:inertia-thB90"
     "fig:magnetic-dust-waves"
     "fig:projected-magnetic-dust-waves-15"
     "fig:projected-magnetic-dust-waves-45"
