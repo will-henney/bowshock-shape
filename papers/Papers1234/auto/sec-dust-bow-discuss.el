1bcebd2351255e15fb31b01bf875e1d437954009
William Henney <will@henney.org>
2019-02-06 18:13:31 -0600

    Write more discussion and add new cloudy dust extinction fig

--- papers/Papers1234/auto/sec-dust-bow-discuss.el
+++ papers/Papers1234/auto/sec-dust-bow-discuss.el
@@ -9,10 +9,12 @@
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-add-symbols
-    "IR")
+    "IR"
+    "pc")
    (LaTeX-add-labels
     "sec:summary-discussion"
     "eq:tau-empirical"
+    "eq:eta-shell"
     "tab:observations"
     "fig:All-sources-eta-tau"
     "sec:case-inside-out"
