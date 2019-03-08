b910942e51d9e7cf476e221c98cf93d1d1d2be19
William Henney <will@henney.org>
2019-03-07 23:19:25 -0600

    Paper II - fix lots of xrefs, reorganize and tidy up

--- papers/Papers1234/auto/app-dust-wave.el
+++ papers/Papers1234/auto/app-dust-wave.el
@@ -1,19 +1,23 @@
 (TeX-add-style-hook
  "app-dust-wave"
  (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (LaTeX-add-labels
     "sec:gas-free-bow"
     "fig:dust-trajectories"
     "eq:dust-rad-force"
     "fn:Qp"
     "eq:dust-r0"
+    "eq:kappa-grain"
     "eq:dust-r-theta"
     "sec:dust-parallel"
     "eq:dust-r-in"
