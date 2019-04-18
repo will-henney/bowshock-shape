8e40ab3f90c907177af6584a95532ae89fe44256
William Henney <will@henney.org>
2019-04-18 11:20:46 -0500

    Lots of reorganizing of Paper IV

--- papers/Papers1234/auto/sec-quadrics-observations.el
+++ papers/Papers1234/auto/sec-quadrics-observations.el
@@ -1,16 +1,19 @@
 (TeX-add-style-hook
  "sec-quadrics-observations"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
-    "sec:comp-with-observ"
     "sec:mid-infrared-arcs"
+    "sec:comp-with-observ"
     "fig:mipsgal-examples"
     "sec:autom-trac-fitt"
     "step:R0"
