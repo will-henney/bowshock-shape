1b2d39a6e89dbbf1bf9bffda57d59f488d318c30
William Henney <will@henney.org>
2019-04-18 22:27:14 -0500

    Finish intro for Paper IV

--- papers/Papers1234/auto/sec-standing-wave.el
+++ papers/Papers1234/auto/sec-standing-wave.el
@@ -1,16 +1,19 @@
 (TeX-add-style-hook
  "sec-standing-wave"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
+    "sec:discussion"
     "fig:perturb-shapes"
-    "fig:perturb-xy-prime"
     "sec:perturbed-bows"
     "eq:standing-wave"
     "eq:fractional-phase"
