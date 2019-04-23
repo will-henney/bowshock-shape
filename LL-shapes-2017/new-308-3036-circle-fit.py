dcdf9c096bc6534ae39c5c1e39af3cc3457d1988
William Henney <will@henney.org>
2019-04-23 11:05:50 -0500

    Finish all the new ridge measurements for LL objects

---
+++ LL-shapes-2017/new-308-3036-circle-fit.py
@@ -0,0 +1,15 @@
+import sys 
+sys.path.append("/Users/will/Dropbox/circle-fit")
+import circle_fit
+
+id_ = "308-3036"
+for arc in ["ridge"]:
+    for delta_theta in 45, 50, 55, 60, 65, 70, 75, 80:
+        plotfile = f"{id_}-{arc}-{delta_theta:02d}.pdf"
+        print('#### '*10)
+        print("Creating", plotfile)
+        circle_fit.plot_solution(
+            f"new-{id_}-{arc}.reg",
+            f"{id_}-Bally_08-extract.fits",
+            plotfile,
+            delta_theta=delta_theta, vmin=1.1, vmax=1.4, sigma=5.0, maxiter=5)
---
+++ LL-shapes-2017/new-4468-685-circle-fit.py
@@ -0,0 +1,15 @@
+import sys 
+sys.path.append("/Users/will/Dropbox/circle-fit")
+import circle_fit
+
+id_ = "4468-685"
+for arc in ["ridge"]:
+    for delta_theta in 45, 50, 55, 60, 65, 70, 75, 80:
+        plotfile = f"{id_}-{arc}-{delta_theta:02d}.pdf"
+        print('#### '*10)
+        print("Creating", plotfile)
+        circle_fit.plot_solution(
+            f"new-{id_}-{arc}.reg",
+            f"{id_}-Bally_17-extract.fits",
+            plotfile,
+            delta_theta=delta_theta, vmin=1.37, vmax=1.55, sigma=3.0, maxiter=5)
