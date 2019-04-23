9aa57ed75a91be721512f6721492da092ce34a7b
William Henney <will@henney.org>
2019-04-22 22:22:08 -0500

    Finish 12/18 of the LL object ridge tracings - six to go!

--- LL-shapes-2017/new-4285-458-circle-fit.py
+++ LL-shapes-2017/new-4285-458-circle-fit.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("/Users/will/Dropbox/circle-fit")
 import circle_fit
 
-id_ = "w069-601"
+id_ = "4285-458"
 for arc in ["ridge"]:
     for delta_theta in 45, 50, 55, 60, 65, 70, 75, 80:
         plotfile = f"{id_}-{arc}-{delta_theta:02d}.pdf"
@@ -10,6 +10,6 @@ for arc in ["ridge"]:
         print("Creating", plotfile)
         circle_fit.plot_solution(
             f"new-{id_}-{arc}.reg",
-            f"{id_}-Bally_01-extract.fits",
+            f"{id_}-Robberto_ACS_1r_f658n-extract.fits",
             plotfile,
-            delta_theta=delta_theta, vmin=6.0, vmax=8.2, sigma=1.5)
+            delta_theta=delta_theta, vmin=1.1, vmax=1.9, sigma=5.0)
--- LL-shapes-2017/new-LL4-circle-fit.py
+++ LL-shapes-2017/new-LL4-circle-fit.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("/Users/will/Dropbox/circle-fit")
 import circle_fit
 
-id_ = "w069-601"
+id_ = "LL4"
 for arc in ["ridge"]:
     for delta_theta in 45, 50, 55, 60, 65, 70, 75, 80:
         plotfile = f"{id_}-{arc}-{delta_theta:02d}.pdf"
@@ -10,6 +10,6 @@ for arc in ["ridge"]:
         print("Creating", plotfile)
         circle_fit.plot_solution(
             f"new-{id_}-{arc}.reg",
-            f"{id_}-Bally_01-extract.fits",
+            f"{id_}-Bally_24-extract.fits",
             plotfile,
-            delta_theta=delta_theta, vmin=6.0, vmax=8.2, sigma=1.5)
+            delta_theta=delta_theta, vmin=0.85, vmax=1.0, sigma=5.0, maxiter=5)
