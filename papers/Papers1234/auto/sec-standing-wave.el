0577ae2e1973edd32b5dfe0451b0688a58bd98dd
William Henney <will@henney.org>
2019-06-28 13:39:33 -0500

    Paper 4 - finish all Jane corrections!

--- papers/Papers1234/auto/sec-standing-wave.el
+++ papers/Papers1234/auto/sec-standing-wave.el
@@ -2,17 +2,21 @@
  "sec-standing-wave"
  (lambda ()
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (LaTeX-add-labels
     "sec:discussion"
-    "fig:perturb-Rc-R90"))
+    "sec:diversity-bow-shock"
+    "sec:infl-proj-effects"
+    "sec:pert-bow-shape"
+    "fig:perturb-Rc-R90"
+    "sec:vari-betw-diff"))
  :latex)
 
