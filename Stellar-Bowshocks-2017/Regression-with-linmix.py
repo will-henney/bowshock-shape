76e1c13d6cf3b2a35be5de8a8050a94b74c377e8
William Henney <will@henney.org>
2020-02-10 09:49:57 -0600

    Finish MCMC regression analysis of Pi-Lambda

--- Stellar-Bowshocks-2017/Regression-with-linmix.py
+++ Stellar-Bowshocks-2017/Regression-with-linmix.py
@@ -48,21 +48,29 @@ tab[m][useful_cols]
 t = tab[m][['Rc', 'Rc_sigma', 'R90', 'R90_sigma']]
 x, xe, y, ye = [np.array(_) for _ in zip(*t.as_array().data)]
 
+# Convert to a log scale.  Calculate the errors explicitly, using centered difference so they are symmetric in log  space.  Use capital letters for log quantities.
+
+X, Y = np.log10(x), np.log10(y)
+Xe = 0.5*(np.log10(x + xe) - np.log10(x - xe))
+Ye = 0.5*(np.log10(y + ye) - np.log10(y - ye))
+
 # And plot the distribution of $\Pi$ and $\Lambda$ with error bars. 
 
 # %matplotlib inline
 from matplotlib import pyplot as plt
 import seaborn as sns
+sns.set_context("talk")
 
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.errorbar(x, y, xerr=xe, yerr=ye, ls=" ", alpha=0.5)
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.errorbar(X, Y, xerr=Xe, yerr=Ye, ls=" ", alpha=0.5)
 ax.set(
-    xlim=[0, None], ylim=[0, None],
-    xlabel=r"$\Pi$", ylabel=r"$\Lambda$",
+    xlim=[-0.5, 0.8], ylim=[-0.5, 0.8],
+    xlabel=r"$\log_{10}\, \Pi$", ylabel=r"$\log_{10}\, \Lambda$",
 )
 ax.set_aspect("equal")
+sns.despine()
 
-lm = linmix.LinMix(x, y, xe, ye, K=2)
+lm = linmix.LinMix(X, Y, Xe, Ye, K=2)
 
 lm.run_mcmc()
 
@@ -104,17 +112,17 @@ sns.pairplot(dfchain, plot_kws=dict(alpha=0.1, s=2),
 
 from scipy.stats import pearsonr
 
-pearsonr(x, y)
+pearsonr(X, Y)
 
-# So this is for the 4,5-star sources.  We get a posterior distribution for the correlation coefficient of $r = 0.57 \pm 0.1$, assuming this is what `lm.chain["corr"]` is.  This is significantly larger than the correlation in observed variables: $r = 0.44$ (see previous cell).
+# So this is for the 4,5-star sources in log space.  We get a posterior distribution for the correlation coefficient of $r = 0.66 \pm 0.09$.  This is significantly larger than the correlation in observed log variables: $r = 0.53$ (see previous cell).
 
-tab[m][useful_cols].to_pandas().describe()
+pd.DataFrame({"X": X, "Xe": Xe, "Y": Y, "Ye": Ye}).describe()
 
-# So note that the observed variable $\Pi$ (`Rc`) is $2.026 \pm 0.777$, whereas the latent variable $\xi$ from the chain posterior is $2.022 \pm 0.770$, which is hardly any different.  
+# So note that the observed variable $\log_{10} \Pi$ (`X`) is $0.279 \pm 0.151$, whereas the latent variable $\xi$ from the chain posterior is $0.283 \pm 0.149$, which is hardly any different.  
 #
-# This is probably because the observaional error bars on $\Pi$ are so small, $0.089 \pm 0.049$.  We should probably increase them, by consideration of different $\theta$ ranges for the circle fits.  In Paper 0 we found values of 0.13 to 0.38, so 0.25 might be a better bet. We could multiply `Rc_sigma` by 3 and see what happens
+# This is probably because the observational error bars on $\Pi$ are so small, $0.021 \pm 0.016$.  We should probably increase them, by consideration of different $\theta$ ranges for the circle fits.  In Paper 0 we found values of 0.13 to 0.38 in linear space, so 0.25 might be a better bet, which corresponds to 0.1 in log space. We could add 0.1 to all the `Xe` and see what happens
 
-lm2 = linmix.LinMix(x, y, 3*xe, ye, K=2, seed=443)
+lm2 = linmix.LinMix(X, Y, Xe+0.1, Ye, K=2, seed=443)
 
 lm2.run_mcmc()
 
@@ -124,9 +132,168 @@ dfchain2 = pd.DataFrame.from_records(
 )
 dfchain2.describe()
 
-# So now we get $2.015 \pm 0.718$, which is hardly any different from before, although the width is very slightly smaller $0.718 \pm 0.084$ instead of $0.771 \pm 0.086$.
+# So now we get $0.290 \pm 0.101$ for the `xi` distribution, which is 30% narrower than before. And we get a much higher latent correlation of $r = 0.89 \pm 0.10$, corresponding to $\sigma = 0.026$ (cf the observed dispersion in `Y` of 0.07). 
 #
-# The uncertainty on the slope has increased, as one would hope, from $0.157$ to $0.167 \pm 0.04$. 
+# Also, the slope $\beta$  has changed significantly from $0.25 \pm 0.04$ to $0.53 \pm 0.12$.  
+
+# Plot the regression
+
+# +
+vmin, vmax = -0.5, 0.8
+xgrid = np.linspace(vmin, vmax, 200)
+
+
+fig, ax = plt.subplots(figsize=(10, 10))
+
+ax.errorbar(X, Y, xerr=Xe+0.1, yerr=Ye, ls=" ", elinewidth=0.2, alpha=1.0, c="k")
+ax.errorbar(X, Y, xerr=Xe, yerr=Ye, ls=" ", elinewidth=0.4, alpha=1.0, c="k")
+ax.scatter(X, Y, marker=".")
+# The original fit
+ax.plot(xgrid, dfchain["alpha"].mean() + xgrid*dfchain["beta"].mean(), 
+        '-', c="k")
+for samp in lm.chain[::20]:
+    ax.plot(xgrid, samp["alpha"] + xgrid*samp["beta"], 
+        '-', c="r", alpha=0.2, lw=0.1)
+# The fit with increased X errors
+ax.plot(xgrid, dfchain2["alpha"].mean() + xgrid*dfchain2["beta"].mean(), 
+        '-', c="k")
+for samp in lm2.chain[::20]:
+    ax.plot(xgrid, samp["alpha"] + xgrid*samp["beta"], 
+        '-', c="m", alpha=0.2, lw=0.1)
+
+ax.set(
+    xlim=[vmin, vmax], ylim=[vmin, vmax],
+    xlabel=r"$\log_{10}\, \Pi$", ylabel=r"$\log_{10}\, \Lambda$",
+)
+ax.set_aspect("equal")
+sns.despine()
+# -
+
+# So that graphically shows the difference between the two fits.  The fit with just the nominal $\Pi$ errors (red) is far too shallow. Allowing for an extra 0.1 dex uncertainty in $\Pi$ (magenta) gives a much steeper slope, which is now consistent with 0.5, as expected for parabolas.  It also has a brader uncertainty in the slope, possible even bimodal.
+
+# The worrying thing is that there is nothing in the method that warns us that the original fit was bad.  I suppose there must be a $\chi^2$ that we could have calculated.  
+
+# Now look at the posterior of fit parameters for the new version.
+
+sns.pairplot(dfchain2, plot_kws=dict(alpha=0.01, s=1), 
+             vars=["alpha", "beta", "corr"])
+
+# Now try the 3-star sources
+
+m3 = tab["Rating"] == 3
+t = tab[m3][['Rc', 'Rc_sigma', 'R90', 'R90_sigma']]
+x, xe, y, ye = [np.array(_) for _ in zip(*t.as_array().data)]
+X3, Y3 = np.log10(x), np.log10(y)
+Xe3 = 0.5*(np.log10(x + xe) - np.log10(x - xe))
+Ye3 = 0.5*(np.log10(y + ye) - np.log10(y - ye))
+
+mgood = np.isfinite(Xe3) & np.isfinite(Ye3)
+X3 = X3[mgood]
+Y3 = Y3[mgood]
+Xe3 = Xe3[mgood]
+Ye3 = Ye3[mgood]
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.errorbar(X3, Y3, xerr=Xe3, yerr=Ye3, ls=" ", c="r", alpha=0.5)
+ax.set(
+    xlim=[-0.5, 0.8], ylim=[-0.5, 0.8],
+    xlabel=r"$\log_{10}\, \Pi$", ylabel=r"$\log_{10}\, \Lambda$",
+)
+ax.set_aspect("equal")
+sns.despine()
+
+lm3 = linmix.LinMix(X3, Y3, Xe3, Ye3, K=2)
+
+lm3.run_mcmc()
+
+dfchain3 = pd.DataFrame.from_records(
+    lm3.chain.tolist(), 
+    columns=lm3.chain.dtype.names
+)
+dfchain3.describe()
+
+pd.DataFrame({"X": X3, "Xe": Xe3, "Y": Y3, "Ye": Ye3}).describe()
+
+pearsonr(X3, Y3)
+
+# So, again we get a flat slope. Let us try with increasing the `Xe3` errors by 0.1 again 
+
+lm3e = linmix.LinMix(X3, Y3, Xe3+0.1, Ye3, K=2)
+lm3e.run_mcmc()
+
+dfchain3e = pd.DataFrame.from_records(
+    lm3e.chain.tolist(), 
+    columns=lm3e.chain.dtype.names
+)
+dfchain3e.describe()
+
+# That is good.  So with this sample too, we now recover a slope of 0.5
+
+# +
+vmin, vmax = -0.5, 0.8
+xgrid = np.linspace(vmin, vmax, 200)
+
+
+fig, ax = plt.subplots(figsize=(10, 10))
+
+ax.errorbar(X3, Y3, xerr=Xe3+0.1, yerr=Ye3, ls=" ", elinewidth=0.2, alpha=1.0, c="k")
+ax.errorbar(X3, Y3, xerr=Xe3, yerr=Ye3, ls=" ", elinewidth=0.4, alpha=1.0, c="k")
+ax.scatter(X3, Y3, marker=".", c="g")
+# The original fit
+ax.plot(xgrid, dfchain3["alpha"].mean() + xgrid*dfchain3["beta"].mean(), 
+        '-', c="k")
+for samp in lm3.chain[::20]:
+    ax.plot(xgrid, samp["alpha"] + xgrid*samp["beta"], 
+        '-', c="r", alpha=0.2, lw=0.1)
+# The fit with increased X errors
+ax.plot(xgrid, dfchain3e["alpha"].mean() + xgrid*dfchain3e["beta"].mean(), 
+        '-', c="k")
+for samp in lm3e.chain[::20]:
+    ax.plot(xgrid, samp["alpha"] + xgrid*samp["beta"], 
+        '-', c="m", alpha=0.2, lw=0.1)
+
+ax.set(
+    xlim=[vmin, vmax], ylim=[vmin, vmax],
+    xlabel=r"$\log_{10}\, \Pi$", ylabel=r"$\log_{10}\, \Lambda$",
+)
+ax.set_aspect("equal")
+sns.despine()
+# -
+
+dfchain3p45 = pd.concat([dfchain2, dfchain3e], keys=["4,5-star", "3-star"])
+
+dfchain3p45
+
+# Using `pd.concat` on the two dataframes gives a multi-level index.  In order to get seaborn's pairplot to distinguish the two, we need to switch that to a categorical column, which we can do with `.reset_index()`: 
+
+dfchain3p45.reset_index(level=0)
+
+sns.pairplot(dfchain3p45[::30].reset_index(), 
+             #kind="reg", 
+             diag_kind="hist",
+             markers=".",
+             plot_kws=dict(marker=".", ec="none", alpha=0.3, s=100), 
+             vars=["alpha", "beta", "corr", "ximean", "xisig"], 
+             hue="level_0",
+             hue_order=["3-star", "4,5-star",]
+            )
+
+# So the above shows that the results from the two data partitions are very similar.  All except for the mean of $\xi$ (the latent planitude)
 #
+#
+# The 95% confidence levels on $\beta$ and the other parameters are:
+#
+
+dfchain2.quantile([0.025, 0.975])
+
+dfchain3e.quantile([0.025, 0.975])
+
+# Or 90% confidence levels of
+
+dfchain2.quantile([0.05, 0.95])
+
+# ## Application to isophotal shape variables
+
+# What we have at the moment are thicknes $h$ and breadth $d\theta$ (which we want to replace with a radius eventually). 
 
 
